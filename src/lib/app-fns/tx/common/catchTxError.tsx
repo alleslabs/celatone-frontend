c54f2c7a41619f6e660c7ec7471ff26fd011cbc0
songwongtp <16089160+songwongtp@users.noreply.github.com>
2022-12-14 16:26:32 +0700

    feat: hash from fail tx, remove see tx if no hash

--- src/lib/app-fns/tx/common/catchTxError.tsx
+++ src/lib/app-fns/tx/common/catchTxError.tsx
@@ -4,49 +4,54 @@ import { IoIosWarning } from "react-icons/io";
 import type { OperatorFunction } from "rxjs";
 import { catchError } from "rxjs";
 
+import { ExplorerLink } from "lib/components/ExplorerLink";
 import type { TxResultRendering } from "lib/types";
 import { TxStreamPhase } from "lib/types";
 import { formatStdFee } from "lib/utils/formatter/denom";
 
-const getErrorResult = (error: Error, fee: StdFee): TxResultRendering => {
-  if (error.message === "Request rejected") {
-    return {
-      value: null,
-      phase: TxStreamPhase.FAILED,
-      receiptInfo: {
-        header: "Request rejected",
-        headerIcon: (
-          <Icon as={IoIosWarning} fontSize="24px" color="error.light" />
-        ),
-      },
-      receipts: [],
-      actionVariant: "rejected",
-    };
-  }
-  return {
-    value: null,
-    phase: TxStreamPhase.FAILED,
-    receiptInfo: {
-      header: "Transaction Failed",
-      description: error.message,
-      headerIcon: (
-        <Icon as={IoIosWarning} fontSize="24px" color="error.light" />
-      ),
-    },
-    receipts: [
-      {
-        title: "Tx Fee",
-        value: formatStdFee(fee),
-      },
-    ],
-  };
-};
-
 export const catchTxError = (
   fee: StdFee
 ): OperatorFunction<TxResultRendering, TxResultRendering> => {
-  // TODO: Figure out how to get txHash
-  return catchError((error) => {
-    return Promise.resolve<TxResultRendering>(getErrorResult(error, fee));
+  return catchError((error: Error) => {
+    const txHash = error.message.match("(?:tx )(.*?)(?= at)")?.at(1);
+
+    return Promise.resolve<TxResultRendering>(
+      error.message === "Request rejected"
+        ? {
+            value: null,
+            phase: TxStreamPhase.FAILED,
+            receiptInfo: {
+              header: "Request rejected",
+              headerIcon: (
+                <Icon as={IoIosWarning} fontSize="24px" color="error.light" />
+              ),
+            },
+            receipts: [],
+            actionVariant: "rejected",
+          }
+        : {
+            value: null,
+            phase: TxStreamPhase.FAILED,
+            receiptInfo: {
+              header: "Transaction Failed",
+              description: error.message,
+              headerIcon: (
+                <Icon as={IoIosWarning} fontSize="24px" color="error.light" />
+              ),
+            },
+            receipts: [
+              {
+                title: "Tx Hash",
+                value: txHash,
+                html: <ExplorerLink type="tx_hash" value={txHash ?? ""} />,
+              },
+              {
+                title: "Tx Fee",
+                value: formatStdFee(fee),
+              },
+            ],
+            actionVariant: !txHash ? "rejected" : undefined,
+          }
+    );
   });
 };
