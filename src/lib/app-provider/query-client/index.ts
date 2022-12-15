298ce9381af98247c9cfcb09ede492f543f913d0
poomthiti <poom.thitipatt@gmail.com>
2022-12-15 17:04:54 +0700

    fix: change gql link for terra 2

--- src/lib/app-provider/query-client/index.ts
+++ src/lib/app-provider/query-client/index.ts
@@ -2,7 +2,11 @@ import { GraphQLClient } from "graphql-request";
 
 export const GRAPH_URL: Record<string, string> = {
   osmosistestnet: "https://osmosis-testnet-graphql.alleslabs.dev/v1/graphql",
-  terra2: "https://phoenix-lcd.terra.dev",
+  /**
+   * Revisit graphql for terra2 mainnet and keplr key for testnet (currently non-existent)
+   */
+  terra2: "https://terra-testnet-graphql.alleslabs.dev/console",
+  terra2testnet: "https://terra-testnet-graphql.alleslabs.dev/console",
 };
 
 export const getIndexerGraphClient = (currentChainName: string) =>
