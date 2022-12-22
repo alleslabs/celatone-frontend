3992249ecaa27b7942197bf6a379f9732a3a3c6a
bkioshn <bkioshn@gmail.com>
2022-12-22 11:22:00 +0700

    fix: remove return

--- src/lib/components/modal/NoMobile.tsx
+++ src/lib/components/modal/NoMobile.tsx
@@ -9,29 +9,27 @@ import {
 } from "@chakra-ui/react";
 import { IoIosWarning } from "react-icons/io";
 
-export const NoMobile = () => {
-  return (
-    <Modal size="sm" isOpen isCentered onClose={() => {}}>
-      <ModalOverlay />
-      <ModalContent>
-        <Flex
-          direction="column"
-          justify="center"
-          align="center"
-          padding="8"
-          textAlign="center"
-        >
-          <Icon as={IoIosWarning} color="error.light" boxSize="50px" />
+export const NoMobile = () => (
+  <Modal size="sm" isOpen isCentered onClose={() => {}}>
+    <ModalOverlay />
+    <ModalContent>
+      <Flex
+        direction="column"
+        justify="center"
+        align="center"
+        padding="8"
+        textAlign="center"
+      >
+        <Icon as={IoIosWarning} color="error.light" boxSize="50px" />
 
-          <Heading as="h6" py="4">
-            Sorry, we currently do not support mobile use.
-          </Heading>
-          <Text variant="body1">
-            Please use Celatone through the desktop browser for the best
-            experience.
-          </Text>
-        </Flex>
-      </ModalContent>
-    </Modal>
-  );
-};
+        <Heading as="h6" py="4">
+          Sorry, we currently do not support mobile use.
+        </Heading>
+        <Text variant="body1">
+          Please use Celatone through the desktop browser for the best
+          experience.
+        </Text>
+      </Flex>
+    </ModalContent>
+  </Modal>
+);
