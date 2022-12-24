63d5158984b7e97d41a774ed5549fd4de5ad9cc8
bkioshn <bkioshn@gmail.com>
2022-12-24 11:22:21 +0700

    feat: render query command shortcut in contract details page

--- src/lib/pages/contract-details/components/CommandSection.tsx
+++ src/lib/pages/contract-details/components/CommandSection.tsx
@@ -1,4 +1,10 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { ButtonGroup, Flex, Spinner, Text } from "@chakra-ui/react";
+import router from "next/router";
+import { useState } from "react";
+
+import { useQueryCmds } from "lib/app-provider/queries/useQueryCmds";
+import ContractCmdButton from "lib/components/ContractCmdButton";
+import { getFirstQueryParam, jsonPrettify } from "lib/utils";
 
 export const CommandSection = () => {
   /**
@@ -6,6 +12,48 @@ export const CommandSection = () => {
    * - Make an interface
    * - Wireup with real query/execute commands data
    */
+  const contractAddress = getFirstQueryParam(router.query.contractAddress);
+
+  const [, setQueryMsg] = useState("");
+
+  const { isFetching: isQueryCmdsFetching, queryCmds } = useQueryCmds({
+    contractAddress,
+  });
+
+  const renderQueryCmds = () => {
+    if (isQueryCmdsFetching) {
+      return <Spinner color="gray.400" size="md" mx={1} />;
+    }
+    if (queryCmds.length) {
+      return (
+        <ButtonGroup
+          width="90%"
+          flexWrap="wrap"
+          rowGap="8px"
+          sx={{
+            "> button": {
+              marginInlineStart: "0 !important",
+              marginInlineEnd: "1",
+            },
+          }}
+        >
+          {queryCmds.map(([cmd, queryMsg]) => (
+            <ContractCmdButton
+              key={`query-cmd-${cmd}`}
+              cmd={cmd}
+              msg={jsonPrettify(queryMsg)}
+              setMsg={setQueryMsg}
+            />
+          ))}
+        </ButtonGroup>
+      );
+    }
+    return (
+      <Text variant="body2" color="text.dark">
+        No messages available
+      </Text>
+    );
+  };
   return (
     <Flex gap={6}>
       <Flex
@@ -19,6 +67,7 @@ export const CommandSection = () => {
           Query Shortcuts
         </Text>
         {/* Query Contract Commands */}
+        {renderQueryCmds()}
       </Flex>
       <Flex
         direction="column"
