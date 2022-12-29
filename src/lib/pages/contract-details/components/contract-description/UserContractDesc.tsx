8a43dd0f4a51f22fea0bec5c89cec3eb343591fc
bkioshn <bkioshn@gmail.com>
2022-12-29 10:58:13 +0700

    fix: add observer

--- src/lib/pages/contract-details/components/contract-description/UserContractDesc.tsx
+++ src/lib/pages/contract-details/components/contract-description/UserContractDesc.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { observer } from "mobx-react-lite";
 import { useState } from "react";
 import { RiPencilFill } from "react-icons/ri";
 import Linkify from "react-linkify";
@@ -13,84 +14,87 @@ import { ShowMoreButton } from "./ShowMoreButton";
 interface UserContractDescProps {
   contractDetail: ContractDetail;
 }
-export const UserContractDesc = ({ contractDetail }: UserContractDescProps) => {
-  const [isUserContractShowMore, setIsUserContractShowMore] = useState(false);
+export const UserContractDesc = observer(
+  ({ contractDetail }: UserContractDescProps) => {
+    const [isUserContractShowMore, setIsUserContractShowMore] = useState(false);
 
-  const [
-    userUserContractRef,
-    {
-      noClamp: noUserDescClamp,
-      clampedText: userContractDesc,
-      key: userContractKey,
-    },
-  ] = useClampText({
-    text: contractDetail.contractInfo?.description || "No contract description",
-    ellipsis: "...",
-    lines: textLine(
-      contractDetail.publicInfo?.description,
-      isUserContractShowMore
-    ),
-  });
+    const [
+      userUserContractRef,
+      {
+        noClamp: noUserDescClamp,
+        clampedText: userContractDesc,
+        key: userContractKey,
+      },
+    ] = useClampText({
+      text:
+        contractDetail.contractInfo?.description || "No contract description",
+      ellipsis: "...",
+      lines: textLine(
+        contractDetail.publicInfo?.description,
+        isUserContractShowMore
+      ),
+    });
 
-  const renderEditContractButton = () => {
-    if (!contractDetail.contractInfo) return null;
-    return (
-      <EditContractDetails
-        contractInfo={contractDetail.contractInfo}
-        triggerElement={
-          <Button
-            size="xs"
-            color="primary.main"
-            variant="none"
-            leftIcon={<RiPencilFill />}
-          >
-            {contractDetail.contractInfo?.description
-              ? "Edit"
-              : "Add Description"}
-          </Button>
-        }
-      />
-    );
-  };
-
-  return (
-    <Flex
-      direction="column"
-      bg="gray.900"
-      borderRadius="8px"
-      p={4}
-      my={6}
-      flex="1"
-      role="group"
-    >
-      <Flex justify="space-between" align="center" h="30px">
-        <Text variant="body2" fontWeight={500} color="text.dark">
-          Your Contract Description
-        </Text>
-        <Box display="none" _groupHover={{ display: "flex" }}>
-          {renderEditContractButton()}
-        </Box>
-      </Flex>
-      <Text
-        variant="body2"
-        ref={userUserContractRef as React.MutableRefObject<HTMLInputElement>}
-        key={userContractKey}
-      >
-        <Linkify>
-          {isUserContractShowMore
-            ? contractDetail.contractInfo?.description
-            : userContractDesc}
-        </Linkify>
-      </Text>
-
-      {!noUserDescClamp && (
-        <ShowMoreButton
-          toggleShowMore={isUserContractShowMore}
-          setToggleShowMore={() =>
-            setIsUserContractShowMore(!isUserContractShowMore)
+    const renderEditContractButton = () => {
+      if (!contractDetail.contractInfo) return null;
+      return (
+        <EditContractDetails
+          contractInfo={contractDetail.contractInfo}
+          triggerElement={
+            <Button
+              size="xs"
+              color="primary.main"
+              variant="none"
+              leftIcon={<RiPencilFill />}
+            >
+              {contractDetail.contractInfo?.description
+                ? "Edit"
+                : "Add Description"}
+            </Button>
           }
         />
-      )}
-    </Flex>
-  );
-};
+      );
+    };
+
+    return (
+      <Flex
+        direction="column"
+        bg="gray.900"
+        borderRadius="8px"
+        p={4}
+        my={6}
+        flex="1"
+        role="group"
+      >
+        <Flex justify="space-between" align="center" h="30px">
+          <Text variant="body2" fontWeight={500} color="text.dark">
+            Your Contract Description
+          </Text>
+          <Box display="none" _groupHover={{ display: "flex" }}>
+            {renderEditContractButton()}
+          </Box>
+        </Flex>
+        <Text
+          variant="body2"
+          ref={userUserContractRef as React.MutableRefObject<HTMLInputElement>}
+          key={userContractKey}
+        >
+          <Linkify>
+            {isUserContractShowMore
+              ? contractDetail.contractInfo?.description
+              : userContractDesc}
+          </Linkify>
+        </Text>
+
+        {!noUserDescClamp && (
+          <ShowMoreButton
+            toggleShowMore={isUserContractShowMore}
+            setToggleShowMore={() =>
+              setIsUserContractShowMore(!isUserContractShowMore)
+            }
+          />
+        )}
+      </Flex>
+    );
+  }
+);
