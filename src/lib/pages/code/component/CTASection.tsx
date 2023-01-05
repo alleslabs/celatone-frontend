fdc65ba85274827505b8c3552d0d77ce0ee6fd13
poomthiti <poom.thitipatt@gmail.com>
2023-01-05 15:02:07 +0700

    feat(components): add instantiate button (WIP: wait for permission enum)

--- src/lib/pages/code/component/CTASection.tsx
+++ src/lib/pages/code/component/CTASection.tsx
@@ -1,8 +1,8 @@
 import { Flex, Button, chakra, Icon } from "@chakra-ui/react";
 import { observer } from "mobx-react-lite";
-import { useRouter } from "next/router";
 import { MdCheck } from "react-icons/md";
 
+import { InstantiateButton } from "lib/components/button/InstantiateButton";
 import { RemoveCode } from "lib/components/modal/code/RemoveCode";
 import { SaveOrEditCodeModal } from "lib/components/modal/code/SaveOrEditCode";
 import { useCodeStore } from "lib/hooks";
@@ -16,21 +16,14 @@ const StyledIcon = chakra(Icon, {
 
 export const CTASection = observer(
   ({ id, ...codeInfo }: Omit<CodeInfo, "contracts">) => {
-    const router = useRouter();
     const { isCodeIdExist } = useCodeStore();
     const isSaved = isCodeIdExist(id);
 
     return (
       <Flex gap={4}>
         {isSaved && <SaveOrEditCodeModal mode="edit" id={id} {...codeInfo} />}
-        <Button
-          variant="outline-primary"
-          onClick={() =>
-            router.push({ pathname: "/instantiate", query: { "code-id": id } })
-          }
-        >
-          Instantiate
-        </Button>
+        {/* TODO: change permission */}
+        <InstantiateButton permission="any" codeId={id} />
         {isSaved ? (
           <RemoveCode
             codeId={id}
