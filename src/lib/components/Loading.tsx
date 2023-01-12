1001fed5e4b60bfe026f80aaaef5b8168a4184a5
Ramida J <jennieramida@gmail.com>
2023-01-12 14:49:59 +0700

    fix(components): fix past transaction misalignment

--- src/lib/components/Loading.tsx
+++ src/lib/components/Loading.tsx
@@ -2,7 +2,14 @@ import { Flex, Spinner, Text } from "@chakra-ui/react";
 
 export const Loading = () => {
   return (
-    <Flex borderY="1px solid" borderColor="divider.main" width="full" py="48px">
+    <Flex
+      borderY="1px solid"
+      borderColor="divider.main"
+      width="full"
+      py={12}
+      flexDirection="column"
+      alignItems="center"
+    >
       <Spinner size="xl" speed="0.65s" />
       <Text mt="20px">Loading ...</Text>
     </Flex>
