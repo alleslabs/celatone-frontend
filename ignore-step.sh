330bd6b3742152104e70cf8e723e9c16d8544bc6
evilpeach <kanisorn.peach@gmail.com>
2023-01-12 12:04:13 +0700

    feat: ignore step

---
+++ ignore-step.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# This script will exit if the branch is not main on Vercel production deploys
+
+if [[ "$VERCEL_GIT_COMMIT_REF" != "main"  ]] ; then
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
+
+else
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+fi
