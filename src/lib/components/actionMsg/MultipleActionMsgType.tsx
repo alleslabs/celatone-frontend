7f7dd8793f494343f2b2a59ed8f64bd6f6c111a7
bkioshn <bkioshn@gmail.com>
2023-01-13 17:52:20 +0700

    style: add comma when multiple actions is rendered

--- src/lib/components/actionMsg/MultipleActionMsgType.tsx
+++ src/lib/components/actionMsg/MultipleActionMsgType.tsx
@@ -8,15 +8,18 @@ interface MultipleActionMsgTypeProps {
 }
 export const MultipleActionMsgType = ({
   messages,
-}: MultipleActionMsgTypeProps) => (
-  <Flex gap={1}>
-    {countMessages(messages).map(
-      (msg) =>
-        msg.count !== 0 && (
-          <Flex key={msg.type} gap={1}>
-            {msg.type} <Tag borderRadius="full">{msg.count}</Tag>
-          </Flex>
-        )
-    )}
-  </Flex>
-);
+}: MultipleActionMsgTypeProps) => {
+  const displayMessagesCount = countMessages(messages).filter(
+    (msg) => msg.count !== 0
+  );
+  return (
+    <Flex gap={1}>
+      {displayMessagesCount.map((msg, index) => (
+        <Flex key={msg.type} gap={1}>
+          {msg.type} <Tag borderRadius="full">{msg.count}</Tag>
+          {index < displayMessagesCount.length - 1 && ","}
+        </Flex>
+      ))}
+    </Flex>
+  );
+};
