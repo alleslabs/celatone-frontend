7a5655e7ca27665ca304616928f31db9b35d062c
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-01-18 22:16:16 +0700

    fix: refactor

--- src/lib/components/ButtonCard.tsx
+++ src/lib/components/ButtonCard.tsx
@@ -15,38 +15,36 @@ export const ButtonCard = ({
   onClick,
   disabled,
   ...componentProps
-}: ButtonCardInterface) => {
-  return (
-    <Flex
-      aria-disabled={disabled}
-      p="24px"
-      align="center"
-      justify="space-between"
-      onClick={!disabled ? onClick : undefined}
-      bgColor="gray.900"
-      borderRadius="4px 4px 0 0"
-      w="100%"
-      cursor="pointer"
-      _hover={{ bgColor: "rgba(255, 255, 255, 0.15)" }}
-      _disabled={{
-        bgColor: "divider.main",
-        cursor: "not-allowed",
-      }}
-      {...componentProps}
-    >
-      <Stack>
-        <Heading
-          as="h6"
-          variant="h6"
-          color={disabled ? "text.disabled" : "text.main"}
-        >
-          {title}
-        </Heading>
-        <Text variant="body2" color={disabled ? "text.disabled" : "text.main"}>
-          {description}
-        </Text>
-      </Stack>
-      <Icon as={FiChevronRight} color="gray.600" fontSize="28px" />
-    </Flex>
-  );
-};
+}: ButtonCardInterface) => (
+  <Flex
+    aria-disabled={disabled}
+    p="24px"
+    align="center"
+    justify="space-between"
+    onClick={!disabled ? onClick : undefined}
+    bgColor="gray.900"
+    borderRadius="4px 4px 0 0"
+    w="100%"
+    cursor="pointer"
+    _hover={{ bgColor: "rgba(255, 255, 255, 0.15)" }}
+    _disabled={{
+      bgColor: "divider.main",
+      cursor: "not-allowed",
+    }}
+    {...componentProps}
+  >
+    <Stack>
+      <Heading
+        as="h6"
+        variant="h6"
+        color={disabled ? "text.disabled" : "text.main"}
+      >
+        {title}
+      </Heading>
+      <Text variant="body2" color={disabled ? "text.disabled" : "text.main"}>
+        {description}
+      </Text>
+    </Stack>
+    <Icon as={FiChevronRight} color="gray.600" fontSize="28px" />
+  </Flex>
+);
