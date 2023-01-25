4dec63a740c64bec858aa636356af6d2222be389
evilpeach <kanisorn.peach@gmail.com>
2023-01-25 17:41:42 +0700

    fix: as comments

--- src/lib/utils/formatter/currency.format.ts
+++ src/lib/utils/formatter/currency.format.ts
@@ -2,14 +2,14 @@ import type { BigSource } from "big.js";
 
 import type { Token, U } from "lib/types";
 
-import { demicrofy } from "./currency";
+import { formatTokenWithPrecision } from "./token";
 
 export function formatToken(
   amount: U<Token<BigSource>>,
   denom: string
 ): string {
   if (denom[0] === "u") {
-    return demicrofy(amount).toFixed(6);
+    return formatTokenWithPrecision(amount, 6);
   }
   return amount.toString();
 }
