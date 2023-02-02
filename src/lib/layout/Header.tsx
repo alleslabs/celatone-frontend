70902e1fb2fa17d30743636afa4380cf4bee02cd
poomthiti <poom.thitipatt@gmail.com>
2023-02-02 19:44:56 +0700

    fix: chain path bug when mixed with query params

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -35,11 +35,12 @@ const Header = () => {
       if (chainName === currentChainName) return;
       setCurrentChain(chainName);
       navigate({
-        pathname: router.asPath.replace(`/${router.query.network}`, ""),
+        pathname: router.pathname.replace("/[network]", ""),
         query: {
           /**
            * @remarks Condition checking varies by chain
            */
+          ...router.query,
           network: getNetworkByChainName(chainName),
         },
       });
