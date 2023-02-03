a7d1dfec1e5b73d2e91729bbdf7befe1b8100480
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-02-03 14:24:57 +0700

    fix: comments

--- src/lib/utils/date.ts
+++ src/lib/utils/date.ts
@@ -7,19 +7,20 @@ import type { Option } from "lib/types";
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
-export const getDefaultDate = (): Date => dayjs(0).toDate();
+export const getDefaultDate = (): Date => dayjs.utc(0).toDate();
 
-export const getCurrentDate = (): Date => dayjs().toDate();
+export const getCurrentDate = (): Date => dayjs.utc().toDate();
 
-export const parseDate = (date: string): Date => dayjs(date).utc(true).toDate();
+export const parseDate = (date: string): Date => dayjs.utc(date).toDate();
 
 export const parseDateOpt = (dateOpt: Option<string>): Option<Date> =>
   dateOpt ? parseDate(dateOpt) : undefined;
 
+// TODO: remove default fn
 export const parseDateDefault = (dateOpt: Option<string>): Date =>
   dateOpt ? parseDate(dateOpt) : getDefaultDate();
 
 export const formatUTC = (date: Date) =>
-  dayjs(date).utc().format("MMM DD, YYYY, h:mm:ss A [(UTC)]");
+  dayjs.utc(date).format("MMM DD, YYYY, h:mm:ss A [(UTC)]");
 
-export const dateFromNow = (date: Date) => dayjs(date).fromNow();
+export const dateFromNow = (date: Date) => dayjs.utc(date).fromNow();
