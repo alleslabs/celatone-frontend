a47964cf9d0bac7ed3c86928cc3ec6b332c28e4c
bkioshn <bkioshn@gmail.com>
2023-02-07 14:56:35 +0700

    refactor: create const for chain id

--- src/lib/hooks/useChainId.ts
+++ src/lib/hooks/useChainId.ts
@@ -2,7 +2,7 @@ import { useWallet } from "@cosmos-kit/react";
 
 export const useChainId = () => {
   const { currentChainRecord } = useWallet();
-  if (!currentChainRecord?.chain.chain_id)
-    throw new Error("Chain ID not found");
-  return currentChainRecord.chain.chain_id;
+  const chainId = currentChainRecord?.chain.chain_id;
+  if (!chainId) throw new Error("Chain ID not found");
+  return chainId;
 };
