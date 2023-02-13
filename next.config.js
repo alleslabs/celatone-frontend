ec13018a8858d3cb0ef2cb58d3fae6e71d06bca1
evilpeach <kanisorn.peach@gmail.com>
2023-02-13 15:21:12 +0700

    fix: remove next-pwa, update next config

--- next.config.js
+++ next.config.js
@@ -1,30 +1,26 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 const { withSentryConfig } = require("@sentry/nextjs");
-const withPWA = require("next-pwa")({
-  dest: "public",
-  disable:
-    process.env.NODE_ENV === "development" ||
-    process.env.NODE_ENV === "preview" ||
-    process.env.NODE_ENV === "production",
-  // delete two lines above to enable PWA in production deployment
-  // add your own icons to public/manifest.json
-  // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-});
 
 /** @type {import('next').NextConfig} */
-module.exports = withPWA({
+const nextConfig = {
   swcMinify: true,
   reactStrictMode: true,
   eslint: {
     dirs: ["src"],
   },
-});
+};
 
-module.exports = withSentryConfig(
-  module.exports,
-  { silent: true, authToken: process.env.SENTRY_AUTH_TOKEN },
-  { hideSourcemaps: true }
-);
+const moduleExports = {
+  ...nextConfig,
+
+  sentry: {
+    hideSourceMaps: true,
+    disableServerWebpackPlugin: false,
+    disableClientWebpackPlugin: false,
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  silent: true,
+};
+
+module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
