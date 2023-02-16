0d6e476c0efbf99a600581bf13aa1eaff0eacd55
bkioshn <bkioshn@gmail.com>
2023-02-16 21:39:36 +0700

    fix: new design for token card

--- src/lib/pages/contract-details/components/token/TokenCard.tsx
+++ src/lib/pages/contract-details/components/token/TokenCard.tsx
@@ -1,31 +1,47 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, Tooltip } from "@chakra-ui/react";
 
 import type { BalanceWithAssetInfo, Token } from "lib/types";
-import { formatTokenWithPrecision } from "lib/utils";
+import { assetValue, formatTokenWithPrecision } from "lib/utils";
 
 interface TokenCardProps {
   userBalance: BalanceWithAssetInfo;
 }
 
-export const TokenCard = ({ userBalance }: TokenCardProps) => (
-  <Flex
-    gap={1}
-    p={2}
-    background="pebble.900"
-    borderRadius="8px"
-    alignItems="center"
-  >
-    <Image
-      boxSize="20px"
-      src={userBalance.assetInfo?.logo}
-      alt={userBalance.balance.symbol}
-    />
-    <Text fontWeight="700">
-      {formatTokenWithPrecision(
-        userBalance.balance.amount as Token,
-        userBalance.balance.precision
-      )}
-    </Text>
-    <Text>{userBalance.balance.symbol}</Text>
-  </Flex>
-);
+export const TokenCard = ({ userBalance }: TokenCardProps) => {
+  const { symbol, price, amount, precision } = userBalance.balance;
+  const tokenWithPrecision = formatTokenWithPrecision(
+    amount as Token,
+    precision
+  );
+
+  return (
+    <Tooltip
+      hasArrow
+      label={price ? `1 ${symbol} = $${price}` : "No Price Data"}
+      placement="top"
+      bg="honeydew.darker"
+      maxW="240px"
+    >
+      <Flex
+        gap={2}
+        p={2}
+        background="pebble.900"
+        borderRadius="8px"
+        alignItems="center"
+      >
+        <Image boxSize={8} src={userBalance.assetInfo?.logo} alt={symbol} />
+        <Box>
+          <Flex gap={1}>
+            <Text fontWeight="700" variant="body2">
+              {tokenWithPrecision}
+            </Text>
+            <Text variant="body2">{symbol}</Text>
+          </Flex>
+          <Text variant="body3" color="text.dark">
+            {price ? `$${assetValue(tokenWithPrecision, price)}` : "-"}
+          </Text>
+        </Box>
+      </Flex>
+    </Tooltip>
+  );
+};
