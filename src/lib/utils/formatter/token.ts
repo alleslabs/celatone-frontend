27ecd0055e862caa75642d4961a99e699393f937
bkioshn <bkioshn@gmail.com>
2023-03-14 22:45:24 +0700

    chore: change var name

--- src/lib/utils/formatter/token.ts
+++ src/lib/utils/formatter/token.ts
@@ -40,16 +40,16 @@ export const toToken = (
 
 /**
  * @remarks
- * If token is more than or equal to 1 billion, should add postfix B and format to 2 decimal point
+ * If token is more than or equal to 1 billion, should add suffix B and format to 2 decimal point
  *
  */
 export const formatUTokenWithPrecision = (
   amount: U<Token<BigSource>>,
   precision: number,
-  isPostfix = true
+  isSuffix = true
 ): string => {
   const token = toToken(amount, precision);
-  if (token.gte(B) && isPostfix) {
+  if (isSuffix && token.gte(B)) {
     return `${formatDemimal({ decimalPoints: 2, delimiter: true })(
       token.div(B),
       "0.00"
