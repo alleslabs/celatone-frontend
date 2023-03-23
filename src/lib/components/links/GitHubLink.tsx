72bd5e2a96dddd23b476288a27634831ba6d914a
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2023-03-23 18:59:54 +0700

    feat(components): add github link to public code and contract

---
+++ src/lib/components/links/GitHubLink.tsx
@@ -0,0 +1,34 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+import { CustomIcon } from "../icon";
+
+interface GitHubLinkProps {
+  github: string;
+}
+
+export const GitHubLink = ({ github }: GitHubLinkProps) => {
+  const [, , , org, repo] = github.split("/");
+  return (
+    <Flex gap={2}>
+      <Text fontWeight={500} color="text.dark" variant="body2">
+        GitHub:
+      </Text>
+      <a
+        href={github}
+        target="_blank"
+        rel="noreferrer"
+        style={{ display: "flex" }}
+      >
+        <Text color="lilac.main" variant="body2" wordBreak="break-all">
+          {org}/{repo}
+        </Text>
+        <CustomIcon
+          className="copier"
+          name="external"
+          boxSize="12px"
+          marginLeft="8px"
+        />
+      </a>
+    </Flex>
+  );
+};
