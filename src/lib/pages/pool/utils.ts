b55bb6c100489186a987ca5ae0fb75e7508ec914
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-04-19 14:53:39 +0700

    fix: comment

--- src/lib/pages/pool/utils.ts
+++ src/lib/pages/pool/utils.ts
@@ -1,8 +1,6 @@
 import { UndefinedTokenList } from "./components/constant";
 
 export const getUndefinedTokenIcon = (denom: string) => {
-  const index = denom.split("").reduce((v, c) => {
-    return v + c.charCodeAt(0);
-  }, 0);
+  const index = denom.split("").reduce((v, c) => v + c.charCodeAt(0), 0);
   return UndefinedTokenList[index % UndefinedTokenList.length];
 };
