fa592dc8e33e0dff5275ec2afb1a6f53295fbf45
bkioshn <bkioshn@gmail.com>
2023-05-11 17:40:00 +0700

    feat: implement flow for store code allowed user

--- src/lib/pages/codes/components/MyStoredCodesSection.tsx
+++ src/lib/pages/codes/components/MyStoredCodesSection.tsx
@@ -1,8 +1,11 @@
-import { Box, Heading, HStack } from "@chakra-ui/react";
+import { Box, Flex, Heading, HStack } from "@chakra-ui/react";
 
+import { useCurrentNetwork } from "lib/app-provider";
 import { MyStoredCodesTable } from "lib/components/table";
 import type { CodeInfo } from "lib/types";
 
+import { DeployButton } from "./DeployButton";
+import { ProposalButton } from "./ProposalButton";
 import { UploadButton } from "./UploadButton";
 
 interface MyStoredCodesSectionProps {
@@ -19,21 +22,34 @@ export const MyStoredCodesSection = ({
   onRowSelect,
   disconnectedMessage,
   isSearching,
-}: MyStoredCodesSectionProps) => (
-  <Box mb={8}>
-    <HStack alignItems="center" justifyContent="space-between" mb="18px">
-      <Heading as="h6" variant="h6">
-        My Stored Codes
-      </Heading>
-      <UploadButton />
-    </HStack>
-    <MyStoredCodesTable
-      codes={codes}
-      isLoading={isLoading}
-      onRowSelect={onRowSelect}
-      emptyMessage="Your uploaded Wasm files will display as My Stored Codes."
-      disconnectedMessage={disconnectedMessage}
-      isSearching={isSearching}
-    />
-  </Box>
-);
+}: MyStoredCodesSectionProps) => {
+  const { isMainnet } = useCurrentNetwork();
+
+  return (
+    <Box mb={8}>
+      <HStack alignItems="center" justifyContent="space-between" mb="18px">
+        <Heading as="h6" variant="h6">
+          My Stored Codes
+        </Heading>
+        <Flex gap={2}>
+          {isMainnet ? (
+            <>
+              <UploadButton />
+              <ProposalButton />
+            </>
+          ) : (
+            <DeployButton />
+          )}
+        </Flex>
+      </HStack>
+      <MyStoredCodesTable
+        codes={codes}
+        isLoading={isLoading}
+        onRowSelect={onRowSelect}
+        emptyMessage="Your uploaded Wasm files will display as My Stored Codes."
+        disconnectedMessage={disconnectedMessage}
+        isSearching={isSearching}
+      />
+    </Box>
+  );
+};
--- src/lib/pages/codes/components/UploadButton.tsx
+++ src/lib/pages/codes/components/UploadButton.tsx
@@ -1,16 +1,41 @@
-import { Button } from "@chakra-ui/react";
+import { Button, Tooltip } from "@chakra-ui/react";
+import { useWallet } from "@cosmos-kit/react";
 
 import { useInternalNavigate } from "lib/app-provider";
 import { CustomIcon } from "lib/components/icon";
+import { useGovParams } from "lib/services/proposalService";
+import type { Addr } from "lib/types";
 
 export const UploadButton = () => {
+  const { address = "" } = useWallet();
   const navigate = useInternalNavigate();
+  const { data: govParams } = useGovParams();
+  const isAllowed = Boolean(
+    govParams?.uploadAccess?.addresses?.includes(address as Addr)
+  );
+
   return (
-    <Button
-      onClick={() => navigate({ pathname: "/upload" })}
-      leftIcon={<CustomIcon name="plus" color="text.main" boxSize="12px" />}
+    <Tooltip
+      hasArrow
+      label="Only allowed address can upload Wasm file without opening proposal"
+      placement="top"
+      arrowSize={8}
+      bg="honeydew.darker"
+      isDisabled={isAllowed}
     >
-      Upload New Code
-    </Button>
+      <Button
+        disabled={!isAllowed}
+        onClick={() => navigate({ pathname: "/upload" })}
+        rightIcon={
+          <CustomIcon
+            name="upload"
+            color={!isAllowed ? "pebble.600" : "text.main"}
+            boxSize="12px"
+          />
+        }
+      >
+        Upload New Code
+      </Button>
+    </Tooltip>
   );
 };
