58533cc013059ac3f5434f9c481878e9d206c0b2
bkioshn <bkioshn@gmail.com>
2023-05-12 11:02:21 +0700

    feat: redirect to deploy if not whitelisted

--- src/lib/pages/upload/index.tsx
+++ src/lib/pages/upload/index.tsx
@@ -1,19 +1,32 @@
 import { Heading, Text } from "@chakra-ui/react";
+import { useWallet } from "@cosmos-kit/react";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
+import { useCurrentNetwork, useInternalNavigate } from "lib/app-provider";
 import { ConnectWalletAlert } from "lib/components/ConnectWalletAlert";
 import { Stepper } from "lib/components/stepper";
 import { UploadSection } from "lib/components/upload/UploadSection";
 import WasmPageContainer from "lib/components/WasmPageContainer";
 import { AmpEvent, AmpTrack } from "lib/services/amplitude";
+import { useGovParams } from "lib/services/proposalService";
+import type { Addr } from "lib/types";
 
 const Upload = () => {
   const router = useRouter();
+  const { isMainnet } = useCurrentNetwork();
+
+  const { address = "" } = useWallet();
+  const navigate = useInternalNavigate();
+  const { data: govParams } = useGovParams();
+  const isAllowed = Boolean(
+    govParams?.uploadAccess?.addresses?.includes(address as Addr)
+  );
 
   useEffect(() => {
-    if (router.isReady) AmpTrack(AmpEvent.TO_UPLOAD);
-  }, [router.isReady]);
+    if (!isAllowed && isMainnet) navigate({ pathname: "/deploy" });
+    else if (router.isReady) AmpTrack(AmpEvent.TO_UPLOAD);
+  }, [isAllowed, isMainnet, navigate, router.isReady]);
 
   return (
     <WasmPageContainer>
