7dba5a2bd7cb1547492586dc7276e7bb11206913
bkioshn <bkioshn@gmail.com>
2023-06-15 16:49:39 +0700

    fix: use api from config

--- src/lib/hooks/useOpenAssetTab.ts
+++ src/lib/hooks/useOpenAssetTab.ts
@@ -1,23 +1,16 @@
-import { useWallet } from "@cosmos-kit/react";
 import { useCallback } from "react";
 
-import { CELATONE_API_ENDPOINT, getChainApiPath } from "env";
-import { useChainId } from "lib/app-provider";
+import { useBaseApiRoute } from "lib/app-provider";
 
 import { openNewTab } from "./useOpenTxTab";
 
 export const useOpenAssetTab = () => {
-  const { currentChainName } = useWallet();
-  const chainId = useChainId();
-
-  const baseUrl = `${CELATONE_API_ENDPOINT}/balances/${getChainApiPath(
-    currentChainName
-  )}/${chainId}`;
+  const balancesApiRoute = useBaseApiRoute("balances");
 
   return useCallback(
     (walletAddr: string) => {
-      openNewTab(`${baseUrl}/${walletAddr}`);
+      openNewTab(`${balancesApiRoute}/${walletAddr}`);
     },
-    [baseUrl]
+    [balancesApiRoute]
   );
 };
