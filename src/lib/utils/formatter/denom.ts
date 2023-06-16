c155ef89e61b931be3e9a483d4338961cb977b94
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-06-16 11:02:54 +0700

    fix: rename old format fn

--- src/lib/utils/formatter/denom.ts
+++ src/lib/utils/formatter/denom.ts
@@ -6,7 +6,7 @@ import type { Fee, Token, U } from "lib/types";
 import { formatToken } from "./currency.format";
 
 // NOTE: revisit this if some chain is not using "u"denom
-const getTokenLabel = (denom: string) => {
+const getTokenLabelByDenom = (denom: string) => {
   if (denom[0] === "u") {
     return denom.replace("u", "").toUpperCase();
   }
@@ -20,12 +20,14 @@ const getTokenLabel = (denom: string) => {
 export const formatUFee = (uFee: string) => {
   const regex = /([0-9]+)|([a-zA-Z]+)/g;
   const [fee, denom] = uFee.match(regex) as RegExpMatchArray;
-  return `${formatToken(fee as U<Token>, denom)} ${getTokenLabel(denom)}`;
+  return `${formatToken(fee as U<Token>, denom)} ${getTokenLabelByDenom(
+    denom
+  )}`;
 };
 
 export const formatStdFee = (fee: StdFee | Fee) => {
   return `${formatToken(
     fee.amount[0].amount as U<Token>,
     fee.amount[0].denom
-  )} ${getTokenLabel(fee.amount[0].denom)}`;
+  )} ${getTokenLabelByDenom(fee.amount[0].denom)}`;
 };
