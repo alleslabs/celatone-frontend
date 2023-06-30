31dc16981fd0c326bac9cedfd18833048792da9a
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-06-30 14:04:02 +0700

    fix: remove beforeEach

--- src/lib/stores/code.test.ts
+++ src/lib/stores/code.test.ts
@@ -14,92 +14,102 @@ test("CodeStore setting userkey", () => {
 
 describe("CodeStore", () => {
   let codeStore: CodeStore;
-  beforeEach(() => {
+  const initCodeStore = () => {
     codeStore = new CodeStore();
     codeStore.setCodeUserKey(TEST_USER_KEY);
-  });
+  };
 
-  test("code local info", () => {
-    // 1. after initialization nothing in the store
-    expect(codeStore.getCodeLocalInfo(1)).toBeUndefined();
-    // -- Do nothing as no code is saved
-    codeStore.removeSavedCode(1);
+  describe("code local info", () => {
+    test("after initialization nothing in the store", () => {
+      initCodeStore();
 
-    // 2. add info for code 1 with no code name
-    codeStore.updateCodeInfo(
-      1,
-      "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr
-    );
-    expect(codeStore.getCodeLocalInfo(1)).toStrictEqual({
-      id: 1,
-      uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+      expect(codeStore.getCodeLocalInfo(1)).toBeUndefined();
+      // Do nothing as no code is saved
+      codeStore.removeSavedCode(1);
     });
 
-    // 3. update name for code 1
-    codeStore.updateCodeInfo(
-      1,
-      "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
-      "code-name"
-    );
-    expect(codeStore.getCodeLocalInfo(1)).toStrictEqual({
-      id: 1,
-      name: "code-name",
-      uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+    test("add info for code 1 with no code name", () => {
+      codeStore.updateCodeInfo(
+        1,
+        "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr
+      );
+      expect(codeStore.getCodeLocalInfo(1)).toStrictEqual({
+        id: 1,
+        uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+      });
     });
 
-    // 4. add code 1 to the saved codes list
-    expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([]);
-    codeStore.saveNewCode(1);
-    expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
-      {
+    test("update name for code 1", () => {
+      codeStore.updateCodeInfo(
+        1,
+        "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
+        "code-name"
+      );
+      expect(codeStore.getCodeLocalInfo(1)).toStrictEqual({
         id: 1,
         name: "code-name",
         uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
-      },
-    ]);
-    // -- There should be no update to another user's last saved codes list
-    expect(codeStore.lastSavedCodes(EMPTY_USER_KEY)).toStrictEqual([]);
+      });
+    });
 
-    // 5. add code 2 (with no info) to the saved codes list
-    // -- Validate code 2 is never saved and have no info
-    expect(codeStore.isCodeIdSaved(2)).toBeFalsy();
-    expect(codeStore.getCodeLocalInfo(2)).toBeUndefined();
+    test("add code 1 to the saved codes list", () => {
+      expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([]);
+      codeStore.saveNewCode(1);
+      expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
+        {
+          id: 1,
+          name: "code-name",
+          uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+        },
+      ]);
+      // There should be no update to another user's last saved codes list
+      expect(codeStore.lastSavedCodes(EMPTY_USER_KEY)).toStrictEqual([]);
+    });
 
-    codeStore.saveNewCode(2);
-    expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
-      {
-        id: 2,
-        name: undefined,
-        uploader: "N/A",
-      },
-      {
-        id: 1,
-        name: "code-name",
-        uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
-      },
-    ]);
+    test("add code 2 (with no info) to the saved codes list", () => {
+      // validate code 2 is never saved and have no info
+      expect(codeStore.isCodeIdSaved(2)).toBeFalsy();
+      expect(codeStore.getCodeLocalInfo(2)).toBeUndefined();
 
-    // 6. update info for code 2
-    codeStore.updateCodeInfo(
-      2,
-      "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
-      ""
-    );
-    expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
-      {
-        id: 2,
-        name: undefined,
-        uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
-      },
-      {
-        id: 1,
-        name: "code-name",
-        uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
-      },
-    ]);
+      codeStore.saveNewCode(2);
+      expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
+        {
+          id: 2,
+          name: undefined,
+          uploader: "N/A",
+        },
+        {
+          id: 1,
+          name: "code-name",
+          uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+        },
+      ]);
+    });
+
+    test("update info for code 2", () => {
+      codeStore.updateCodeInfo(
+        2,
+        "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
+        ""
+      );
+      expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
+        {
+          id: 2,
+          name: undefined,
+          uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+        },
+        {
+          id: 1,
+          name: "code-name",
+          uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
+        },
+      ]);
+    });
   });
 
   test("save codes", () => {
+    initCodeStore();
+
     expect(codeStore.isCodeIdSaved(1)).toBeFalsy();
     expect(codeStore.lastSavedCodeIds(TEST_USER_KEY)).toStrictEqual([]);
 
