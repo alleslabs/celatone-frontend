6125de3847dfef294fa53e4da14a51fc588344a6
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-06-30 12:51:53 +0700

    fix: add more comment

--- src/lib/stores/code.test.ts
+++ src/lib/stores/code.test.ts
@@ -20,10 +20,12 @@ describe("CodeStore", () => {
   });
 
   test("code local info", () => {
+    // 1. after initialization nothing in the store
     expect(codeStore.getCodeLocalInfo(1)).toBeUndefined();
-    // Do nothing as no code is saved
+    // -- Do nothing as no code is saved
     codeStore.removeSavedCode(1);
 
+    // 2. add info for code 1 with no code name
     codeStore.updateCodeInfo(
       1,
       "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr
@@ -33,6 +35,7 @@ describe("CodeStore", () => {
       uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
     });
 
+    // 3. update name for code 1
     codeStore.updateCodeInfo(
       1,
       "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
@@ -43,8 +46,8 @@ describe("CodeStore", () => {
       name: "code-name",
       uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
     });
-    expect(codeStore.getCodeLocalInfo(2)).toBeUndefined();
 
+    // 4. add code 1 to the saved codes list
     expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([]);
     codeStore.saveNewCode(1);
     expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
@@ -54,10 +57,14 @@ describe("CodeStore", () => {
         uploader: "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly",
       },
     ]);
-
+    // -- There should be no update to another user's last saved codes list
     expect(codeStore.lastSavedCodes(EMPTY_USER_KEY)).toStrictEqual([]);
 
+    // 5. add code 2 (with no info) to the saved codes list
+    // -- Validate code 2 is never saved and have no info
     expect(codeStore.isCodeIdSaved(2)).toBeFalsy();
+    expect(codeStore.getCodeLocalInfo(2)).toBeUndefined();
+
     codeStore.saveNewCode(2);
     expect(codeStore.lastSavedCodes(TEST_USER_KEY)).toStrictEqual([
       {
@@ -72,6 +79,7 @@ describe("CodeStore", () => {
       },
     ]);
 
+    // 6. update info for code 2
     codeStore.updateCodeInfo(
       2,
       "celat1r02tlyyaqs6tmrfa4jf37t7ewuxr57qp8ghzly" as Addr,
