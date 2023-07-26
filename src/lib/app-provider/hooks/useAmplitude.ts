31fb5bf5ecfb9d76c65f813b2c791b88b4a5dc31
bkioshn <bkioshn@gmail.com>
2023-07-26 10:33:53 +0700

    fix: add networks to amp

--- src/lib/app-provider/hooks/useAmplitude.ts
+++ src/lib/app-provider/hooks/useAmplitude.ts
@@ -12,7 +12,6 @@ export const useAmplitude = ({
   chainName: Option<string>;
   navOpen: boolean;
   devMode: Option<boolean>;
-  // eslint-disable-next-line sonarjs/cognitive-complexity
 }) => {
   /**
    * @remarks Revisit default chain name
@@ -27,27 +26,32 @@ export const useAmplitude = ({
       serverUrl: "/amplitude",
     });
 
-    const wallets = localStorage.getItem("wallets");
-    if (wallets) {
-      if (address) {
-        const walletsJson = JSON.parse(wallets);
-        const addressHash = createHash("sha256").update(address).digest("hex");
+    const wallets = JSON.parse(localStorage.getItem("wallets") ?? "[]");
 
-        if (!walletsJson.includes(addressHash)) {
-          walletsJson.push(addressHash);
-          localStorage.setItem("wallets", JSON.stringify(walletsJson));
-        }
+    if (address) {
+      const addressHash = createHash("sha256").update(address).digest("hex");
+
+      if (!wallets.includes(addressHash)) {
+        wallets.push(addressHash);
       }
-    } else {
-      localStorage.setItem("wallets", JSON.stringify([]));
     }
+    localStorage.setItem("wallets", JSON.stringify(wallets));
+
+    const networks = JSON.parse(localStorage.getItem("networks") ?? "[]");
+
+    if (chainName && !networks.includes(chainName)) {
+      networks.push(chainName);
+    }
+    localStorage.setItem("networks", JSON.stringify(networks));
 
     // Custom user properties
     const identifyEvent = new Identify();
-    identifyEvent.set("Wallets", wallets ?? "");
-    identifyEvent.set("Wallets Number", JSON.parse(wallets ?? "").length);
+    identifyEvent.set("Wallets", wallets);
+    identifyEvent.set("Wallets Count", wallets.length);
     identifyEvent.set("Nav Open", navOpen);
     identifyEvent.set("Dev Mode", devMode ?? "");
+    identifyEvent.set("Networks", networks);
+    identifyEvent.set("Networks Count", networks.length);
     identify(identifyEvent);
   }
 };
