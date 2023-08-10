9118a0eab17cd23bc999df00a9961ccd82202b51
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-08-10 11:44:59 +0700

    fix: remove dummy

--- src/lib/pages/home/index.tsx
+++ src/lib/pages/home/index.tsx
@@ -7,7 +7,6 @@ import { CURR_THEME } from "env";
 import { useCelatoneApp, useInternalNavigate } from "lib/app-provider";
 import { ConnectWalletAlert } from "lib/components/ConnectWalletAlert";
 import { CustomIcon } from "lib/components/icon";
-import { DummyJsonSchema } from "lib/components/json-schema";
 import PageContainer from "lib/components/PageContainer";
 import { ViewMore } from "lib/components/table";
 import { Tooltip } from "lib/components/Tooltip";
@@ -142,7 +141,6 @@ const Home = () => {
 
   return (
     <PageContainer>
-      <DummyJsonSchema />
       <Flex
         direction="column"
         p={{ base: 3, md: 12 }}
