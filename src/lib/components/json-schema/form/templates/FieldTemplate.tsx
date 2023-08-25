155c5dbeea49cecf4db10d801d7d2e0a3ce69abf
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-08-25 16:37:28 +0700

    fix: remove default required string

--- src/lib/components/json-schema/form/templates/FieldTemplate.tsx
+++ src/lib/components/json-schema/form/templates/FieldTemplate.tsx
@@ -39,7 +39,6 @@ export default function FieldTemplate<T = any, F = any>(
       if (required) {
         if (schema.type === "array") onChange([] as T);
         else if (schema.type === "boolean") onChange(false as T);
-        else if (schema.type === "string") onChange("" as T);
       } else onChange(null as T);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
