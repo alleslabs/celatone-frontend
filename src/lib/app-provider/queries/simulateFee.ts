ff499d6d12b782b67a182b87e0e26e45611f70eb
evilpeach <kanisorn.peach@gmail.com>
2023-08-28 17:27:49 +0700

    fix: redo

--- src/lib/app-provider/queries/simulateFee.ts
+++ src/lib/app-provider/queries/simulateFee.ts
@@ -64,7 +64,7 @@ export const useSimulateFeeQuery = ({
       CELATONE_QUERY_KEYS.SIMULATE_FEE,
       chain.chain_name,
       userAddress,
-      JSON.stringify(messages),
+      messages,
       rpcEndpoint,
     ],
     queryFn: async () => simulateFn(messages),
