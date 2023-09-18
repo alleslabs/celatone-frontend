a659b5c1405cb89c392ed5b30a5e17fb713e8802
evilpeach <kanisorn.peach@gmail.com>
2023-09-18 16:54:11 +0700

    fix: use pathname instead of asPath

--- src/lib/app-provider/hooks/usePreviousPathname.ts
+++ src/lib/app-provider/hooks/usePreviousPathname.ts
@@ -2,13 +2,13 @@ import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
 
 export const usePreviousPathname = () => {
-  const { asPath } = useRouter();
+  const { pathname } = useRouter();
 
   const ref = useRef<string | null>(null);
 
   useEffect(() => {
-    ref.current = asPath;
-  }, [asPath]);
+    ref.current = pathname;
+  }, [pathname]);
 
   return ref.current;
 };
