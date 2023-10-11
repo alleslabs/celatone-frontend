371ca120aad2f67f22d98139e319aa53883132db
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-10-11 11:14:55 +0700

    fix: rename

--- src/lib/components/EstimatedFeeRender.tsx
+++ src/lib/components/EstimatedFeeRender.tsx
@@ -15,23 +15,18 @@ export const EstimatedFeeRender = ({
     withPrices: false,
   });
 
-  if (isAssetInfoLoading) {
-    return <Spinner size="sm" mx={1} />;
-  }
-
-  if (loading) {
+  if (isAssetInfoLoading) return <Spinner size="sm" mx={1} />;
+  if (loading)
     return (
       <>
         <Spinner size="sm" mx={1} /> Estimating ...
       </>
     );
-  }
-  const coin = estimatedFee?.amount.at(0);
 
+  const coin = estimatedFee?.amount.at(0);
   if (!coin) return <>--</>;
 
   const chainAssetInfo = assetInfos?.[coin.denom];
-
   return (
     <>
       {formatBalanceWithDenom({
