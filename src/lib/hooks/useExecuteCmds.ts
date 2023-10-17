0f5de7c4c8b9f392d4cc204485b94e1e5317d4f2
evilpeach <kanisorn.peach@gmail.com>
2023-10-17 15:11:14 +0700

    chore: refactor for readiness

--- src/lib/hooks/useExecuteCmds.ts
+++ src/lib/hooks/useExecuteCmds.ts
@@ -26,23 +26,26 @@ export const useExecuteCmds = (contractAddress: ContractAddr) => {
     ],
     retry: false,
     onError: (e) => {
+      const executeCmds: string[] = [];
+
       // Check if Sylvia framework
       const sylviaRegex =
         /Messages supported by this contract: (.*?): execute wasm contract failed: invalid request/;
       const contentMatch = e.message?.match(sylviaRegex);
-      const executeCmds: string[] = [];
 
       if (contentMatch && contentMatch[1]) {
         const content = contentMatch[1].split(",");
         content.forEach((each) => executeCmds.push(each.trim()));
-        setExecCmds(executeCmds.map((cmd) => [cmd, `{"${cmd}": {}}`]));
       } else if (e.message.includes("Error parsing into type")) {
         Array.from(e.message?.matchAll(/`(.*?)`/g) || [])
           .slice(1)
           .forEach((match) => executeCmds.push(match[1]));
-        setExecCmds(executeCmds.map((cmd) => [cmd, `{"${cmd}": {}}`]));
-      } else {
+      }
+
+      if (executeCmds.length === 0) {
         setExecCmds([["", "{}"]]);
+      } else {
+        setExecCmds(executeCmds.map((cmd) => [cmd, `{"${cmd}": {}}`]));
       }
     },
   });
