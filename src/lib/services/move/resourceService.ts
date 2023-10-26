4969778f21449ce4dc53b877542cb0950d263848
evilpeach <kanisorn.peach@gmail.com>
2023-10-26 14:26:57 +0700

    chore: fix import

--- src/lib/services/move/resourceService.ts
+++ src/lib/services/move/resourceService.ts
@@ -2,11 +2,11 @@ import type { QueryFunction, UseQueryResult } from "@tanstack/react-query";
 import { useQuery } from "@tanstack/react-query";
 
 import { CELATONE_QUERY_KEYS, useBaseApiRoute } from "lib/app-provider";
-import type { MoveAccountAddr } from "lib/types";
 import type {
   ResourceGroup,
   ResourceGroupByAccount,
-} from "lib/types/move/resource";
+  MoveAccountAddr,
+} from "lib/types";
 import { truncate } from "lib/utils";
 
 import { getAccountResources } from "./resource";
