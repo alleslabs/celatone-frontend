d61e8aaec61b7dabf652268c94ffa356f49b41b6
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-11-02 18:39:55 +0700

    fix: refactor tokenwithvalue

--- src/lib/types/asset.ts
+++ src/lib/types/asset.ts
@@ -2,25 +2,24 @@ import type Big from "big.js";
 
 import type { LPDetails, Option, Token, U, USD } from "lib/types";
 
-export type TokenWithValue =
-  | {
-      isLPToken: false;
-      denom: string;
-      amount: U<Token<Big>>;
-      symbol: Option<string>;
-      logo: Option<string>;
-      precision: Option<number>;
-      price: Option<USD<Big>>;
-      value: Option<USD<Big>>;
-    }
-  | {
-      isLPToken: true;
-      denom: string;
-      amount: U<Token<Big>>;
-      symbol: Option<string>;
-      logo: Option<string[]>;
-      precision: Option<number>;
-      price: Option<USD<Big>>;
-      value: Option<USD<Big>>;
-      lpDetails: LPDetails;
-    };
+interface BaseTokenWithValue {
+  denom: string;
+  amount: U<Token<Big>>;
+  symbol: Option<string>;
+  precision: Option<number>;
+  price: Option<USD<Big>>;
+  value: Option<USD<Big>>;
+}
+
+export type TokenWithValue = BaseTokenWithValue &
+  (
+    | {
+        isLPToken: false;
+        logo: Option<string>;
+      }
+    | {
+        isLPToken: true;
+        logo: Option<string[]>;
+        lpDetails: LPDetails;
+      }
+  );
