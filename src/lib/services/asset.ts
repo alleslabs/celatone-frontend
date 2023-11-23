bf4bed5d80c51ef323ac8871e73773e5b2ea8480
songwongtp <16089160+songwongtp@users.noreply.github.com>
2023-11-23 16:58:25 +0700

    fix: sentry console error

--- src/lib/services/asset.ts
+++ src/lib/services/asset.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
 import { z } from "zod";
 
-import { AssetInfoSchema, type AssetInfo } from "lib/types";
+import type { AssetInfo } from "lib/types";
+import { AssetInfoSchema } from "lib/types";
 
 export const getAssetInfos = async (
   baseApiRoute: string,
   withPrices: boolean
-): Promise<AssetInfo[]> => {
-  return axios
+): Promise<AssetInfo[]> =>
+  axios
     .get(`${baseApiRoute}`, {
       params: {
         with_prices: withPrices,
       },
     })
     .then((res) => z.array(AssetInfoSchema).parse(res.data));
-};
