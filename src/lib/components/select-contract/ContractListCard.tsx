76f9e85ae7a869f92206df1055cf5c0c3548aaa4
Jennie Alles <jennieramida@gmail.com>
2023-12-20 18:22:48 +0700

    fix(components): remove flex

--- src/lib/components/select-contract/ContractListCard.tsx
+++ src/lib/components/select-contract/ContractListCard.tsx
@@ -36,72 +36,74 @@ export const ContractListCard = ({
       h="75px"
       onClick={() => handleListSelect(item.slug)}
       isDisabled={isDisabled}
+      gap={4}
+      w="full"
+      alignItems="center"
+      justifyContent="flex-start"
     >
-      <Flex gap={4} w="full" alignItems="center">
-        <CustomIcon
-          name={getListIcon(item.name)}
-          boxSize="24px"
-          color="gray.600"
-        />
-        <Flex
-          flexDirection="column"
-          alignItems="start"
-          gap={1}
-          w="full"
-          maxW="calc(100% - 108px)"
-        >
-          <Flex alignItems="center" gap={2} w="full">
-            <Text
-              variant="body1"
-              textColor={isDisabled ? "text.disabled" : "text.main"}
-              fontWeight={700}
-              textOverflow="ellipsis"
-              overflow="hidden"
-            >
-              {item.name}
-            </Text>
-            <Badge>{item.contracts.length}</Badge>
-          </Flex>
-          {isInstantiatedByMe && (
-            <Text
-              variant="body3"
-              textColor={isDisabled ? "text.disabled" : "text.dark"}
-            >
-              Updated {dateFromNow(item.lastUpdated)}
-            </Text>
-          )}
+      <CustomIcon
+        name={getListIcon(item.name)}
+        boxSize="24px"
+        color="gray.600"
+      />
+      <Flex
+        flexDirection="column"
+        alignItems="start"
+        gap={1}
+        w="full"
+        maxW="calc(100% - 108px)"
+      >
+        <Flex alignItems="center" gap={2} w="full">
+          <Text
+            variant="body1"
+            textColor={isDisabled ? "text.disabled" : "text.main"}
+            fontWeight={700}
+            textOverflow="ellipsis"
+            overflow="hidden"
+          >
+            {item.name}
+          </Text>
+          <Badge>{item.contracts.length}</Badge>
         </Flex>
-        {!isReadOnly && (
-          <Menu>
-            <MenuButton
-              m={0}
-              size="sm"
-              h="full"
-              variant="ghost-gray"
-              as={Button}
-              onClick={(e) => e.stopPropagation()}
-            >
-              <CustomIcon name="more" color="gray.600" />
-            </MenuButton>
-            <MenuList onClick={(e) => e.stopPropagation()}>
-              <EditListNameModal
-                list={{ label: item.name, value: item.slug }}
-                menuItemProps={{
-                  icon: <CustomIcon name="edit" color="gray.600" />,
-                  children: "Edit list name",
-                }}
-              />
-              <RemoveListModal
-                list={{ label: item.name, value: item.slug }}
-                menuItemProps={{
-                  icon: <CustomIcon name="delete" color="error.light" />,
-                  children: "Remove list",
-                }}
-              />
-            </MenuList>
-          </Menu>
+        {isInstantiatedByMe && (
+          <Text
+            variant="body3"
+            textColor={isDisabled ? "text.disabled" : "text.dark"}
+          >
+            Updated {dateFromNow(item.lastUpdated)}
+          </Text>
         )}
       </Flex>
+      {!isReadOnly && (
+        <Menu>
+          <MenuButton
+            m={0}
+            size="sm"
+            h="full"
+            variant="ghost-gray"
+            as={Button}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <CustomIcon name="more" color="gray.600" />
+          </MenuButton>
+          <MenuList onClick={(e) => e.stopPropagation()}>
+            <EditListNameModal
+              list={{ label: item.name, value: item.slug }}
+              menuItemProps={{
+                icon: <CustomIcon name="edit" color="gray.600" />,
+                children: "Edit list name",
+              }}
+            />
+            <RemoveListModal
+              list={{ label: item.name, value: item.slug }}
+              menuItemProps={{
+                icon: <CustomIcon name="delete" color="error.light" />,
+                children: "Remove list",
+              }}
+            />
+          </MenuList>
+        </Menu>
+      )}
     </Flex>
   );
 };
