7906a93774813339f96bb71f751bb3921e10a7d5
evilpeach <kanisorn.peach@gmail.com>
2023-12-21 14:58:20 +0700

    fix: add useMemo

--- src/lib/components/module/ModuleCard.tsx
+++ src/lib/components/module/ModuleCard.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Flex, Grid, Text } from "@chakra-ui/react";
+import { useMemo } from "react";
 
 import { AppLink } from "../AppLink";
 import { CustomIcon } from "../icon";
@@ -26,49 +28,60 @@ export const ModuleCard = ({
     moduleName: module.moduleName,
   });
 
-  const card = (
-    <Grid
-      borderRadius={8}
-      bgColor={
-        module.moduleName === selectedModule?.moduleName
-          ? "gray.700"
-          : "gray.800"
-      }
-      p={4}
-      alignItems="center"
-      cursor="pointer"
-      onClick={() => setSelectedModule?.(module)}
-      gap={1}
-      templateColumns="20px 1fr auto"
-      _hover={{
-        bg: "gray.700",
-      }}
-      transition=".25s all ease-out"
-    >
-      <CustomIcon name="contract-address" color="primary.main" boxSize={3} />
-      <Flex align="center" overflow="hidden" gap={1}>
-        <Text className="ellipsis" variant="body2">
-          {module.moduleName}
-        </Text>
-        {isVerified && (
-          <CustomIcon
-            name="check-circle-solid"
-            color="success.main"
-            boxSize={3}
+  const card = useMemo(
+    () => (
+      <Grid
+        borderRadius={8}
+        bgColor={
+          module.moduleName === selectedModule?.moduleName
+            ? "gray.700"
+            : "gray.800"
+        }
+        p={4}
+        alignItems="center"
+        cursor="pointer"
+        onClick={() => setSelectedModule?.(module)}
+        gap={1}
+        templateColumns="20px 1fr auto"
+        _hover={{
+          bg: "gray.700",
+        }}
+        transition=".25s all ease-out"
+      >
+        <CustomIcon name="contract-address" color="primary.main" boxSize={3} />
+        <Flex align="center" overflow="hidden" gap={1}>
+          <Text className="ellipsis" variant="body2">
+            {module.moduleName}
+          </Text>
+          {isVerified && (
+            <CustomIcon
+              name="check-circle-solid"
+              color="success.main"
+              boxSize={3}
+            />
+          )}
+        </Flex>
+        <Flex gap={1} ml="auto">
+          <CountBadge count={module.viewFunctions.length} variant="view" />
+          <CountBadge
+            count={module.executeFunctions.length}
+            variant="execute"
           />
-        )}
-      </Flex>
-      <Flex gap={1} ml="auto">
-        <CountBadge count={module.viewFunctions.length} variant="view" />
-        <CountBadge count={module.executeFunctions.length} variant="execute" />
-      </Flex>
-    </Grid>
+        </Flex>
+      </Grid>
+    ),
+    [
+      isVerified,
+      JSON.stringify(module),
+      selectedModule?.moduleName,
+      setSelectedModule,
+    ]
   );
 
   return setSelectedModule ? (
     card
   ) : (
-    <AppLink href={`/modules/${module.address}/${module.moduleName}`} passHref>
+    <AppLink href={`/modules/${module.address}/${module.moduleName}`}>
       {card}
     </AppLink>
   );
