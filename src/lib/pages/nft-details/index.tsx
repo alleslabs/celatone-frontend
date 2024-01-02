8b6b536d639d3caf949ca3b1c1e38b9c865d205c
James0126 <tubleyes@gmail.com>
2024-01-02 17:45:01 +0900

    fix: fix nft address

--- src/lib/pages/nft-details/index.tsx
+++ src/lib/pages/nft-details/index.tsx
@@ -157,7 +157,10 @@ const NFTDetail = () => {
                     <Text color="gray.400" fontWeight={500} whiteSpace="nowrap">
                       NFT Address:
                     </Text>
-                    <ExplorerLink value={nftAddress} type="user_address" />
+                    <ExplorerLink
+                      value={encodeURIComponent(nftAddress)}
+                      type="user_address"
+                    />
                   </Flex>
                   <Flex gap="8px">
                     <Text color="gray.400" fontWeight={500} whiteSpace="nowrap">
