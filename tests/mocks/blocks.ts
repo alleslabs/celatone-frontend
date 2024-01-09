c15b4b59ece193d64ca2fd5d3283238032b3e4ca
evilpeach <kanisorn.peach@gmail.com>
2024-01-09 19:10:52 +0700

    feat: setup playwright, blocks snapshot

---
+++ tests/mocks/blocks.ts
@@ -0,0 +1,118 @@
+const mockBlocksResponse = {
+  items: [
+    {
+      hash: "\\x6f99e19f6dcb91d124cd5f968c2cc82531b281ffeeb7d417fd64f4894a560c3b",
+      height: 13151707,
+      timestamp: "2024-01-08T04:51:06.037362",
+      transaction_count: 8,
+      validator: {
+        identity: "",
+        moniker: "Larry Engineer",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xaeea6b9ea38ce7928e9cc40b902c8b780d98aad77a2cf68dbb145a608329726e",
+      height: 13151706,
+      timestamp: "2024-01-08T04:51:00.995297",
+      transaction_count: 23,
+      validator: {
+        identity: "",
+        moniker: " Lavender.Five Nodes",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xc29b9c4e53905087ec40dff674338439853f2f775b3db8272cfbba5173e90950",
+      height: 13151705,
+      timestamp: "2024-01-08T04:50:55.97078",
+      transaction_count: 15,
+      validator: {
+        identity: "",
+        moniker: "Zero Knowledge Validator (ZKV)",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xfd85ab98b4d8ea44f5fc30daeec7c0d2b7ed365a8d9d31f9a4e94f3e2bece9ed",
+      height: 13151704,
+      timestamp: "2024-01-08T04:50:50.43911",
+      transaction_count: 23,
+      validator: {
+        identity: "",
+        moniker: "defiantlabs",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xbfeec43c618b67d13acf9678839d6c1824f4dbd4264b349322a242ed73f90d9b",
+      height: 13151703,
+      timestamp: "2024-01-08T04:50:47.549033",
+      transaction_count: 4,
+      validator: {
+        identity: "",
+        moniker: "Astroquirks | Profit Sharing \ud83e\udd11 \ud83d\udcb0",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xb97036c1f599aba09fabe2a33ddb49ae75d4976b22aa1bdb2bbb541f45cc7162",
+      height: 13151702,
+      timestamp: "2024-01-08T04:50:41.553911",
+      transaction_count: 0,
+      validator: {
+        identity: "",
+        moniker: "PRYZM | Stake & Earn",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xff6b4c7aaa7c565a7a53b558a09f43f4c725117e2cabdb86d32ffa6f7017c92e",
+      height: 13151701,
+      timestamp: "2024-01-08T04:50:38.545866",
+      transaction_count: 6,
+      validator: {
+        identity: "",
+        moniker: "ECO Stake \ud83c\udf31",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\x5ed9d4dde30c3ef8c5cb91f5daf45603ae52acfe2c7401571dfc85ff25195ed0",
+      height: 13151700,
+      timestamp: "2024-01-08T04:50:33.591776",
+      transaction_count: 20,
+      validator: {
+        identity: "",
+        moniker: "Crosnest",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\xf9be589d7cc330331e13794ba5822d8ff21a6004703839d51d9310a86033d43f",
+      height: 13151699,
+      timestamp: "2024-01-08T04:50:27.660374",
+      transaction_count: 3,
+      validator: {
+        identity: "",
+        moniker: "ChainLayer",
+        operator_address: "mock",
+      },
+    },
+    {
+      hash: "\\x5df483b1e36806dbdb78a176d6b710628c95468205931efaeae8722bbf812375",
+      height: 13151698,
+      timestamp: "2024-01-08T04:50:21.385011",
+      transaction_count: 24,
+      validator: {
+        identity: "",
+        moniker: "Chandra Station ",
+        operator_address: "mock",
+      },
+    },
+  ],
+  total: 13151707,
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { mockBlocksResponse };
