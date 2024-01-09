f6e6575054d32570788ac102e51dc94ce3d6a5c1
evilpeach <kanisorn.peach@gmail.com>
2024-01-09 23:21:06 +0700

    fix: refactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ const BASE_URL = process.env.BASE_URL || `http://localhost:3000`;
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./playwright-tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
