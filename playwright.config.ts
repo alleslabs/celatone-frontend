ce141cb47a9df073e5a595346e6273cc0185f1c4
evilpeach <kanisorn.peach@gmail.com>
2024-01-09 22:53:51 +0700

    fix: update config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const BASE_URL = process.env.BASE_URL || `http://localhost:3000`;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -25,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:3000",
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,7 +68,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "yarn dev",
-    url: "http://127.0.0.1:3000",
+    url: BASE_URL,
     reuseExistingServer: !process.env.CI,
   },
 });
