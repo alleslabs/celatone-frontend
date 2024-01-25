3aee001fe5f8a90d1e37d286cdd007c9a2d119f3
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-01-25 21:43:09 +0700

    fix: empty case fn

--- src/lib/utils/tx/extractTxLogs.ts
+++ src/lib/utils/tx/extractTxLogs.ts
@@ -2,24 +2,31 @@ import type { Event, logs } from "@cosmjs/stargate";
 
 import type { TxResponse } from "lib/services/tx";
 
+interface Log extends logs.Log {
+  events: Event[];
+}
+
 export const extractTxLogs = (txData: TxResponse): logs.Log[] => {
   if (txData.logs.length > 0) return txData.logs;
 
-  const logs: Record<string, logs.Log & { events: Event[] }> = {};
+  const logs: Record<string, Log> = {};
   txData.events.forEach((event) => {
-    const attribute = event.attributes.find((attr) => attr.key === "msg_index");
-    if (attribute) {
-      const index = attribute.value;
-      if (!Object.keys(logs).includes(attribute.value)) {
+    const index = event.attributes.find(
+      (attr) => attr.key === "msg_index"
+    )?.value;
+    if (index) {
+      if (!logs[index])
         logs[index] = {
           msg_index: Number(index),
           log: "",
-          events: [] as Event[],
+          events: [],
         };
-      }
+
       logs[index].events.push(event);
     }
   });
 
-  return Object.values(logs).toSorted((a, b) => a.msg_index - b.msg_index);
+  const msgLogs = Object.values(logs);
+  msgLogs.sort((a, b) => a.msg_index - b.msg_index);
+  return msgLogs;
 };
