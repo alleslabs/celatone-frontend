86a8796b1a11c6b570cb342c297cf27dee783450
Poafs1 <prach@alleslabs.com>
2024-02-07 13:43:32 +0700

    test(utils): add unit test for account store (mobx)

---
+++ src/lib/stores/account.test.ts
@@ -0,0 +1,67 @@
+import { zBechAddr } from "lib/types";
+
+import { AccountStore } from "./account";
+
+jest.mock("mobx-persist-store", () => ({
+  isHydrated: jest.fn().mockReturnValue(true),
+  makePersistable: jest.fn(),
+}));
+
+describe("accountStore", () => {
+  let accountStore: AccountStore;
+
+  beforeEach(() => {
+    accountStore = new AccountStore();
+  });
+
+  test("user key management", () => {
+    expect(accountStore.isAccountUserKeyExist()).toBeFalsy();
+
+    accountStore.setAccountUserKey("user-key");
+
+    expect(accountStore.isAccountUserKeyExist()).toBeTruthy();
+  });
+
+  test("account info", () => {
+    expect(
+      accountStore.getAccountLocalInfo(zBechAddr.parse("address"))
+    ).toBeUndefined();
+
+    accountStore.updateAccountLocalInfo(
+      zBechAddr.parse("address"),
+      "name",
+      "description"
+    );
+
+    expect(
+      accountStore.getAccountLocalInfo(zBechAddr.parse("address"))
+    ).toEqual({
+      address: zBechAddr.parse("address"),
+      name: "name",
+      description: "description",
+    });
+  });
+
+  test("saved account", () => {
+    expect(accountStore.savedAccounts).toEqual({});
+    expect(accountStore.isAccountSaved(zBechAddr.parse("address"))).toBeFalsy();
+
+    accountStore.updateAccountLocalInfo(
+      zBechAddr.parse("address"),
+      "name",
+      "description"
+    );
+
+    expect(
+      accountStore.isAccountSaved(zBechAddr.parse("address"))
+    ).toBeTruthy();
+
+    expect(accountStore.getSavedAccounts()).toEqual([
+      {
+        address: zBechAddr.parse("address"),
+        name: "name",
+        description: "description",
+      },
+    ]);
+  });
+});
