29d512332913618b52d3e07bfefbb8f731ee9967
Jennie Alles <jennieramida@gmail.com>
2024-02-13 13:59:10 +0700

    feat(components): refactor proposal message card

--- src/lib/pages/proposal-details/components/proposal-overview/ProposalMessageCard.tsx
+++ src/lib/pages/proposal-details/components/proposal-overview/ProposalMessageCard.tsx
@@ -18,53 +18,43 @@ interface ProposalMessageCardProps {
 export const ProposalMessageCard = ({
   header,
   jsonString,
-}: ProposalMessageCardProps) => {
-  return (
-    <AccordionItem mb={4}>
-      {({ isExpanded }) => (
-        <>
-          <AccordionButton
-            background="gray.900"
-            borderRadius={8}
-            _hover={{ background: "gray.900" }}
-            onClick={() =>
-              trackUseExpand({
-                action: !isExpanded ? "expand" : "collapse",
-                component: "proposal_message_card",
-                section: "proposal message",
-              })
-            }
+}: ProposalMessageCardProps) => (
+  <AccordionItem mb={4}>
+    {({ isExpanded }) => (
+      <>
+        <AccordionButton
+          background="gray.900"
+          borderRadius={8}
+          _hover={{ background: "gray.900" }}
+          onClick={() =>
+            trackUseExpand({
+              action: !isExpanded ? "expand" : "collapse",
+              component: "proposal_message_card",
+              section: "proposal message",
+            })
+          }
+        >
+          <Flex
+            px={4}
+            justifyContent="space-between"
+            w="full"
+            align="center"
+            className="copier-wrapper"
           >
-            <Flex
-              px={4}
-              justifyContent="space-between"
-              w="full"
-              align="center"
-              className="copier-wrapper"
-            >
-              <Flex alignItems="center">
-                <Text variant="body1" fontWeight={600} wordBreak="break-word">
-                  {header}
-                </Text>
-              </Flex>
-              <Flex alignItems="center" gap={2}>
-                <AccordionIcon color="gray.600" />
-              </Flex>
+            <Flex alignItems="center">
+              <Text variant="body1" fontWeight={600} wordBreak="break-word">
+                {header}
+              </Text>
             </Flex>
-          </AccordionButton>
-          <AccordionPanel p={0} pt={4}>
-            <div
-              style={
-                isExpanded
-                  ? { display: "block" }
-                  : { height: 0, display: "none" }
-              }
-            >
-              <JsonReadOnly text={jsonString} canCopy isExpandable />
-            </div>
-          </AccordionPanel>
-        </>
-      )}
-    </AccordionItem>
-  );
-};
+            <Flex alignItems="center" gap={2}>
+              <AccordionIcon color="gray.600" />
+            </Flex>
+          </Flex>
+        </AccordionButton>
+        <AccordionPanel p={0} pt={4}>
+          <JsonReadOnly text={jsonString} canCopy isExpandable />
+        </AccordionPanel>
+      </>
+    )}
+  </AccordionItem>
+);
