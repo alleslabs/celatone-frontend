b48dc0def5e5ae1d6ef9d9d9c38192a7db338d04
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-03-04 18:16:42 +0700

    feat: api v1 validator info

--- src/lib/services/validatorService.ts
+++ src/lib/services/validatorService.ts
@@ -12,8 +12,13 @@ import {
 } from "lib/app-provider";
 import type { Validator, ValidatorAddr, Nullable, Option } from "lib/types";
 
-import type { ValidatorsResponse } from "./validator";
-import { resolveValIdentity, getValidator, getValidators } from "./validator";
+import type { ValidatorDataResponse, ValidatorsResponse } from "./validator";
+import {
+  resolveValIdentity,
+  getValidator,
+  getValidators,
+  getValidatorData,
+} from "./validator";
 
 export const useValidator = (
   validatorAddr: ValidatorAddr,
@@ -90,6 +95,18 @@ export const useValidators = (
     {
       retry: 1,
       ...options,
+    }
+  );
+};
+
+export const useValidatorData = (validatorAddress: ValidatorAddr) => {
+  const endpoint = useBaseApiRoute("validators");
+
+  return useQuery<ValidatorDataResponse>(
+    [CELATONE_QUERY_KEYS.VALIDATOR_DATA, endpoint, validatorAddress],
+    async () => getValidatorData(endpoint, validatorAddress),
+    {
+      retry: 1,
     }
   );
 };
