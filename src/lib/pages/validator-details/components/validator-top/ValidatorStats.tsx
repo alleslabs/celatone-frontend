d1f93b3423645303315698daaad957631e6bf6b1
Poafs1 <prach@alleslabs.com>
2024-03-12 17:11:21 +0700

    feat(components): fix stat top overview section for mobile layout

--- src/lib/pages/validator-details/components/validator-top/ValidatorStats.tsx
+++ src/lib/pages/validator-details/components/validator-top/ValidatorStats.tsx
@@ -1,6 +1,7 @@
-import { Flex, Spinner, Text } from "@chakra-ui/react";
+import { Box, Flex, Spinner, Text } from "@chakra-ui/react";
 import type { ReactNode } from "react";
 
+import { useCelatoneApp } from "lib/app-provider";
 import { DotSeparator } from "lib/components/DotSeparator";
 import {
   useValidatorDelegators,
@@ -37,12 +38,19 @@ export const ValidatorStats = ({
   const { data: stakingProvisions } = useValidatorStakingProvisions();
   const { data: delegations, isLoading: isDelegationsLoading } =
     useValidatorDelegators(info.validatorAddress);
+  const {
+    chainConfig: {
+      extra: { singleStakingDenom },
+    },
+  } = useCelatoneApp();
 
   if (!stakingProvisions) return <Spinner size="sm" />;
 
-  const estimatedApr = stakingProvisions.stakingProvisions.div(
-    totalVotingPower.mul(1 - info.commissionRate)
-  );
+  const estimatedApr = singleStakingDenom
+    ? stakingProvisions.stakingProvisions.div(
+        totalVotingPower.mul(1 - info.commissionRate)
+      )
+    : undefined;
 
   const delegationsValue =
     !isDelegationsLoading && delegations ? (
@@ -52,26 +60,31 @@ export const ValidatorStats = ({
     );
 
   return (
-    <Flex
+    <Box
+      display={{ base: "grid", md: "flex" }}
       alignItems="center"
-      justifyContent={{ base: "space-between", md: "start" }}
-      gap={{ base: 3, md: 2 }}
+      gap={2}
+      gridTemplateColumns={`repeat(${singleStakingDenom ? 3 : 2}, 1fr)`}
+      border={{ base: "1px solid", md: "0px" }}
+      borderColor="gray.700"
+      borderRadius={4}
       mt={{ base: 2, md: 1 }}
       mb={{ base: 0, md: 1 }}
       px={{ base: 3, md: 0 }}
       py={{ base: 1, md: 0 }}
-      border={{ base: "1px solid", md: "0px" }}
-      borderColor="gray.700"
-      borderRadius={4}
     >
       <StatWithLabel label="Commission" value={`${info.commissionRate}%`} />
-      <DotSeparator bg="gray.600" display={{ base: "none", md: "flex" }} />
-      <StatWithLabel
-        label="Estimated APR"
-        value={`${String(estimatedApr.toNumber())}%`}
-      />
+      {estimatedApr && (
+        <>
+          <DotSeparator bg="gray.600" display={{ base: "none", md: "flex" }} />
+          <StatWithLabel
+            label="Estimated APR"
+            value={`${String(estimatedApr.toNumber())}%`}
+          />
+        </>
+      )}
       <DotSeparator bg="gray.600" display={{ base: "none", md: "flex" }} />
       <StatWithLabel label="Delegators" value={delegationsValue} />
-    </Flex>
+    </Box>
   );
 };
