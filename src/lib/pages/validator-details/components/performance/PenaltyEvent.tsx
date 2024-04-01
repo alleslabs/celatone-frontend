2e63c2787ce87320e23294543af6b844451bbee0
evilpeach <kanisorn.peach@gmail.com>
2024-04-01 15:41:44 +0700

    fix: add icon for unjailed

--- src/lib/pages/validator-details/components/performance/PenaltyEvent.tsx
+++ src/lib/pages/validator-details/components/performance/PenaltyEvent.tsx
@@ -1,4 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
+import { useMemo } from "react";
 
 import { ExplorerLink } from "lib/components/ExplorerLink";
 import { CustomIcon } from "lib/components/icon";
@@ -8,23 +9,31 @@ import { SlashingEvent } from "lib/types";
 interface PenaltyEventProps {
   event: ValidatorUptimeResponse["events"][0];
 }
-export const PenaltyEvent = ({ event }: PenaltyEventProps) => (
-  <Flex alignItems="center" gap={2}>
-    <Flex alignItems="center" gap={1}>
-      <CustomIcon
-        name="alert-triangle"
-        color={
-          event.type === SlashingEvent.Jailed ? "warning.main" : "error.main"
-        }
-      />
-      <Text variant="body2" color="text.main">
-        {event.type} at block height
-      </Text>
-      <ExplorerLink
-        type="block_height"
-        value={event.height.toString()}
-        showCopyOnHover
-      />
+export const PenaltyEvent = ({ event }: PenaltyEventProps) => {
+  const { icon, color } = useMemo(() => {
+    switch (event.type) {
+      case SlashingEvent.Slashed:
+        return { icon: "alert-triangle" as const, color: "error.main" };
+      case SlashingEvent.Jailed:
+        return { icon: "alert-triangle" as const, color: "warning.main" };
+      default:
+        return { icon: "info-circle" as const, color: "secondary.main" };
+    }
+  }, [event.type]);
+
+  return (
+    <Flex alignItems="center" gap={2}>
+      <Flex alignItems="center" gap={1}>
+        <CustomIcon name={icon} color={color} />
+        <Text variant="body2" color="text.main">
+          {event.type} at block height
+        </Text>
+        <ExplorerLink
+          type="block_height"
+          value={event.height.toString()}
+          showCopyOnHover
+        />
+      </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
