3934d62bbf46d7770acc12e9b73f51972ecb890f
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-04-25 12:18:02 +0700

    fix: change initia to tomcat

--- src/config/chain/initia.ts
+++ src/config/chain/initia.ts
@@ -7,19 +7,18 @@ const INITIA_DECODER =
   "https://initia-api-jiod42ec2q-as.a.run.app/decode_module";
 
 export const INITIA_CHAIN_CONFIGS: ChainConfigs = {
-  "mahalo-2-lite": {
+  "tomcat-1-lite": {
     tier: "lite",
     chain: "initia",
-    registryChainName: "initiadevnet2",
-    prettyName: "Initia Closed Testnet 2 Lite",
-    lcd: "https://lcd.mahalo-2.initia.xyz",
-    rpc: "https://rpc.mahalo-2.initia.xyz:443",
-    indexer: "https://mahalo-2-graphql.alleslabs.dev/v1/graphql",
+    registryChainName: "blackwingdevnet1",
+    prettyName: "Blackwing Closed Testnet 1 Lite",
+    lcd: "https://maze-rest-c64ef367-a391-4e71-901e-87951e5e9030.ue1-prod.newmetric.xyz",
+    rpc: "https://maze-rpc-c64ef367-a391-4e71-901e-87951e5e9030.ue1-prod.newmetric.xyz",
+    indexer: "https://tomcat-1-graphql.alleslabs.dev/v1/graphql",
     wallets: [...initiaWallets, ...keplrWallets],
     features: {
       faucet: {
         enabled: false,
-        // url: process.env.NEXT_PUBLIC_INITIA_DEVNET_FAUCET_URL ?? "",
       },
       wasm: {
         enabled: false,
@@ -28,7 +27,7 @@ export const INITIA_CHAIN_CONFIGS: ChainConfigs = {
         enabled: true,
         moduleMaxFileSize: 1_048_576,
         decodeApi: INITIA_DECODER,
-        verify: "https://compiler.mahalo-2.initia.xyz/contracts/verify",
+        verify: "",
       },
       pool: {
         enabled: false,
@@ -37,9 +36,7 @@ export const INITIA_CHAIN_CONFIGS: ChainConfigs = {
         enabled: true,
       },
       gov: {
-        enabled: true,
-        version: "v1",
-        hideOpenProposal: true,
+        enabled: false,
       },
       nft: {
         enabled: true,
@@ -48,12 +45,15 @@ export const INITIA_CHAIN_CONFIGS: ChainConfigs = {
     gas: {
       gasPrice: {
         tokenPerGas: 0.151,
-        denom: "uinit",
+        denom:
+          "l2/ad2344d0c17127cc6bce67e360f43cd6c5fa09a7b5f6f9b7b80f9dc3e0c4876e",
       },
       gasAdjustment: 1.5,
       maxGasLimit: 25_000_000,
     },
-    extra: {},
+    extra: {
+      isValidatorExternalLink: null,
+    },
   },
   "mahalo-2": {
     tier: "full",
