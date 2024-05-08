a10c1785ccef2fd413a14649aa4f30511e284e25
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-05-08 17:31:22 +0700

    fix: comment out osmosis

--- src/config/theme/index.ts
+++ src/config/theme/index.ts
@@ -1,12 +1,12 @@
 import { DEFAULT_THEME } from "./default";
 import { INITIA_THEME } from "./initia";
-import { OSMOSIS_THEME } from "./osmosis";
+// import { OSMOSIS_THEME } from "./osmosis";
 import { SEI_THEME } from "./sei";
 
 export const getTheme = (chain: string) => {
   switch (chain) {
-    case "osmosis":
-      return OSMOSIS_THEME;
+    // case "osmosis":
+    //   return OSMOSIS_THEME;
     case "sei":
       return SEI_THEME;
     case "initia":
