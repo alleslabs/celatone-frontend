65c11040a0451000e4814fc35fb4871e6d886806
Jennie Alles <jennieramida@gmail.com>
2024-05-15 18:30:52 +0700

    fix(components): fix module top

--- src/lib/pages/module-details/components/ModuleTop.tsx
+++ src/lib/pages/module-details/components/ModuleTop.tsx
@@ -1,5 +1,5 @@
 import type { TextProps } from "@chakra-ui/react";
-import { Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { Button, Flex, Grid, GridItem, Heading, Text } from "@chakra-ui/react";
 import { useMemo } from "react";
 
 import { AmpEvent, track } from "lib/amplitude";
@@ -86,195 +86,198 @@ export const ModuleTop = ({ moduleData, isVerified }: ModuleTopProps) => {
           { text: moduleData.moduleName },
         ]}
       />
-      <Flex
-        justify="space-between"
+      <Grid
+        gridTemplateColumns="minmax(auto, 1fr) minmax(520px, auto)"
         mt={{ base: 3, md: 6 }}
-        direction={{ base: "column", md: "row" }}
-        gap={{ md: 4 }}
       >
-        <Flex
-          direction="column"
-          textOverflow="ellipsis"
-          gap={{ base: 2, md: 1 }}
-        >
+        <GridItem>
           <Flex
-            gap={1}
-            align={{ base: "start", md: "center" }}
-            maxW={{ md: "670px" }}
+            direction="column"
+            textOverflow="ellipsis"
+            gap={{ base: 2, md: 1 }}
           >
-            <CustomIcon
-              name="contract-address"
-              boxSize={5}
-              color="secondary.main"
-            />
-            <Heading
-              as="h5"
-              mt={{ base: 1, md: 0 }}
-              ml={{ base: 1, md: 0 }}
-              variant={{ base: "h6", md: "h5" }}
-              className={!isMobile ? "ellipsis" : ""}
+            <Flex
+              gap={1}
+              align={{ base: "start", md: "center" }}
+              maxW={{ md: "640px" }}
             >
-              {moduleData.moduleName}
-            </Heading>
-            {isVerified && (
-              <Tooltip label="This module's verification is supported by its provided source code.">
-                <Flex>
-                  <CustomIcon
-                    name="check-circle-solid"
-                    boxSize={5}
-                    color="success.main"
-                  />
-                </Flex>
-              </Tooltip>
-            )}
-          </Flex>
-          <Flex
-            mt={{ base: 2, md: 2 }}
-            gap={{ base: 0, md: 2 }}
-            direction={{ base: "column", md: "row" }}
-          >
-            <Text {...baseTextStyle} color="text.main">
-              Module Path:
-            </Text>
-            <CopyLink
-              value={`${moduleData.address}::${moduleData.moduleName}`}
-              amptrackSection="module_top"
-              type="module_path"
-            />
-          </Flex>
-          <Flex
-            mt={{ base: 2, md: 0 }}
-            gap={{ base: 0, md: 2 }}
-            direction={{ base: "column", md: "row" }}
-          >
-            <Text {...baseTextStyle} color="text.main">
-              Creator:
-            </Text>
-            <ExplorerLink
-              value={moduleAddress}
-              ampCopierSection="module_top"
-              textFormat="normal"
-              maxWidth="fit-content"
-              type="user_address"
-              fixedHeight={false}
-            />
-          </Flex>
-          <Flex
-            mt={{ base: 2, md: 0 }}
-            gap={{ base: 0, md: 2 }}
-            direction={{ base: "column", md: "row" }}
-          >
-            <Text {...baseTextStyle} color="text.main" mt="1px">
-              Friends:
-            </Text>
-            <Flex gap={1}>
-              {moduleData.parsedAbi.friends.length ? (
-                <Flex
-                  display="inline"
-                  sx={{
-                    "> p:last-child > span": {
-                      display: "none",
-                    },
-                  }}
-                >
-                  {moduleData.parsedAbi.friends.map((item) => (
-                    <Text key={item} {...baseTextStyle} display="inline-flex">
-                      {item}
-                      <span>,&nbsp;</span>
-                    </Text>
-                  ))}
-                </Flex>
-              ) : (
-                <Text {...baseTextStyle}>-</Text>
+              <CustomIcon
+                name="contract-address"
+                boxSize={5}
+                color="secondary.main"
+              />
+              <Heading
+                as="h5"
+                mt={{ base: 1, md: 0 }}
+                ml={{ base: 1, md: 0 }}
+                variant={{ base: "h6", md: "h5" }}
+                className={!isMobile ? "ellipsis" : ""}
+              >
+                {moduleData.moduleName}
+              </Heading>
+              {isVerified && (
+                <Tooltip label="This module's verification is supported by its provided source code.">
+                  <Flex>
+                    <CustomIcon
+                      name="check-circle-solid"
+                      boxSize={5}
+                      color="success.main"
+                    />
+                  </Flex>
+                </Tooltip>
               )}
             </Flex>
+            <Flex
+              mt={{ base: 2, md: 2 }}
+              gap={{ base: 0, md: 2 }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Text {...baseTextStyle} color="text.main">
+                Module Path:
+              </Text>
+              <CopyLink
+                value={`${moduleData.address}::${moduleData.moduleName}`}
+                amptrackSection="module_top"
+                type="module_path"
+              />
+            </Flex>
+            <Flex
+              mt={{ base: 2, md: 0 }}
+              gap={{ base: 0, md: 2 }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Text {...baseTextStyle} color="text.main">
+                Creator:
+              </Text>
+              <ExplorerLink
+                value={moduleAddress}
+                ampCopierSection="module_top"
+                textFormat="normal"
+                maxWidth="fit-content"
+                type="user_address"
+                fixedHeight={false}
+              />
+            </Flex>
+            <Flex
+              mt={{ base: 2, md: 0 }}
+              gap={{ base: 0, md: 2 }}
+              direction={{ base: "column", md: "row" }}
+            >
+              <Text {...baseTextStyle} color="text.main" mt="1px">
+                Friends:
+              </Text>
+              <Flex gap={1}>
+                {moduleData.parsedAbi.friends.length ? (
+                  <Flex
+                    display="inline"
+                    sx={{
+                      "> p:last-child > span": {
+                        display: "none",
+                      },
+                    }}
+                  >
+                    {moduleData.parsedAbi.friends.map((item) => (
+                      <Text key={item} {...baseTextStyle} display="inline-flex">
+                        {item}
+                        <span>,&nbsp;</span>
+                      </Text>
+                    ))}
+                  </Flex>
+                ) : (
+                  <Text {...baseTextStyle}>-</Text>
+                )}
+              </Flex>
+            </Flex>
           </Flex>
-        </Flex>
-        <Flex
-          gap={{ base: 2, md: 4 }}
-          mt={{ base: 8, md: 0 }}
-          w={{ base: "full", md: "auto" }}
-        >
-          {!isMobile && (
-            <>
-              <Button
-                variant="outline-white"
-                w={{ base: "full", md: "auto" }}
-                leftIcon={<CustomIcon name="query" mr={0} />}
-                size={{ base: "sm", md: "md" }}
-                onClick={() => {
-                  track(AmpEvent.USE_MODULE_DETAILS_MAIN_CTA, {
-                    label: "view",
-                  });
-                  navigate({
-                    pathname: "/interact",
-                    query: {
-                      address: moduleData.address,
-                      moduleName: moduleData.moduleName,
-                      functionType: "view",
-                    },
-                  });
-                }}
-              >
-                View
-              </Button>
-              <Button
-                variant="outline-white"
-                w={{ base: "full", md: "auto" }}
-                leftIcon={<CustomIcon name="execute" mr={0} />}
-                size={{ base: "sm", md: "md" }}
-                onClick={() => {
-                  track(AmpEvent.USE_MODULE_DETAILS_MAIN_CTA, {
-                    label: "execute",
-                  });
-                  navigate({
-                    pathname: "/interact",
-                    query: {
-                      address: moduleData.address,
-                      moduleName: moduleData.moduleName,
-                      functionType: "execute",
-                    },
-                  });
-                }}
-              >
-                Execute
-              </Button>
-              <Tooltip
-                variant="primary-light"
-                label={republishRemark}
-                closeOnClick={false}
-              >
+        </GridItem>
+        <GridItem>
+          <Flex
+            gap={{ base: 2, md: 3 }}
+            mt={{ base: 8, md: 0 }}
+            w={{ base: "full", md: "auto" }}
+            justifyContent="end"
+          >
+            {!isMobile && (
+              <>
                 <Button
-                  isDisabled={!canRepublish}
                   variant="outline-white"
                   w={{ base: "full", md: "auto" }}
-                  leftIcon={<CustomIcon name="migrate" mr={0} />}
+                  leftIcon={<CustomIcon name="query" mr={0} />}
                   size={{ base: "sm", md: "md" }}
                   onClick={() => {
                     track(AmpEvent.USE_MODULE_DETAILS_MAIN_CTA, {
-                      label: "republish",
+                      label: "view",
                     });
                     navigate({
-                      pathname: "/publish-module",
+                      pathname: "/interact",
+                      query: {
+                        address: moduleData.address,
+                        moduleName: moduleData.moduleName,
+                        functionType: "view",
+                      },
                     });
                   }}
                 >
-                  Republish
+                  View
                 </Button>
-              </Tooltip>
-            </>
-          )}
-          <CopyButton
-            amptrackSection="[Module Detail CTA] Copy ABI "
-            value={moduleData.abi}
-            variant="outline-primary"
-            size={{ base: "sm", md: "md" }}
-            buttonText="Copy ABI"
-            iconGap={2}
-            w={{ base: "full", md: "auto" }}
-          />
-        </Flex>
-      </Flex>
+                <Button
+                  variant="outline-white"
+                  w={{ base: "full", md: "auto" }}
+                  leftIcon={<CustomIcon name="execute" mr={0} />}
+                  size={{ base: "sm", md: "md" }}
+                  onClick={() => {
+                    track(AmpEvent.USE_MODULE_DETAILS_MAIN_CTA, {
+                      label: "execute",
+                    });
+                    navigate({
+                      pathname: "/interact",
+                      query: {
+                        address: moduleData.address,
+                        moduleName: moduleData.moduleName,
+                        functionType: "execute",
+                      },
+                    });
+                  }}
+                >
+                  Execute
+                </Button>
+                <Tooltip
+                  variant="primary-light"
+                  label={republishRemark}
+                  closeOnClick={false}
+                >
+                  <Button
+                    isDisabled={!canRepublish}
+                    variant="outline-white"
+                    w={{ base: "full", md: "auto" }}
+                    leftIcon={<CustomIcon name="migrate" mr={0} />}
+                    size={{ base: "sm", md: "md" }}
+                    onClick={() => {
+                      track(AmpEvent.USE_MODULE_DETAILS_MAIN_CTA, {
+                        label: "republish",
+                      });
+                      navigate({
+                        pathname: "/publish-module",
+                      });
+                    }}
+                  >
+                    Republish
+                  </Button>
+                </Tooltip>
+              </>
+            )}
+            <CopyButton
+              amptrackSection="[Module Detail CTA] Copy ABI "
+              value={moduleData.abi}
+              variant="outline-primary"
+              size={{ base: "sm", md: "md" }}
+              buttonText="Copy ABI"
+              iconGap={2}
+              w={{ base: "full", md: "auto" }}
+            />
+          </Flex>
+        </GridItem>
+      </Grid>
     </Flex>
   );
 };
