0797581f29a580a6eef82a4050ec6d5aa192f9e1
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-05-17 14:22:30 +0700

    fix: tolowercase

--- src/lib/hooks/useEaster.ts
+++ src/lib/hooks/useEaster.ts
@@ -6,7 +6,7 @@ import { useCelatoneApp } from "lib/app-provider";
 export const useEaster = (keyword: string) => {
   const { currentChainId, setTheme } = useCelatoneApp();
   useEffect(() => {
-    if (currentChainId === "initiation-1" && keyword === "jennie")
+    if (currentChainId === "initiation-1" && keyword.toLowerCase() === "jennie")
       setTheme(JENNIE_THEME);
   }, [currentChainId, keyword, setTheme]);
 };
