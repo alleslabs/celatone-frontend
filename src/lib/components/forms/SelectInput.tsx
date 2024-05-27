d1ec68b84f0e517f7b9407529ee3d91324d19518
evilpeach <kanisorn.peach@gmail.com>
2024-05-27 23:08:19 +0700

    fix: asset input bug

--- src/lib/components/forms/SelectInput.tsx
+++ src/lib/components/forms/SelectInput.tsx
@@ -29,7 +29,7 @@ interface SelectInputProps<T extends string> {
   }[];
   onChange: (newVal: T) => void;
   placeholder?: string;
-  initialSelected: string;
+  initialSelected?: string;
   hasDivider?: boolean;
   helperTextComponent?: ReactNode;
   labelBgColor?: string;
@@ -81,11 +81,14 @@ export const SelectInput = <T extends string>({
   const selectedOption = options.find((item) => item.label === selected);
 
   useEffect(() => {
+    if (initialSelected) return;
+
     setSelected(
       options.find((item) => !item.disabled && item.value === initialSelected)
         ?.label ?? ""
     );
-  }, [initialSelected, options]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [initialSelected]);
 
   return (
     <Popover
@@ -162,6 +165,7 @@ export const SelectInput = <T extends string>({
             borderBottomColor: hasDivider && "gray.700",
           },
         }}
+        overflow="scroll"
       >
         {options.map(({ label, value, disabled, icon, iconColor, image }) => (
           <SelectItem
