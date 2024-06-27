94cc51bdc8ef99f29c04ea8a3cd48547929db02a
evilpeach <kanisorn.peach@gmail.com>
2024-06-27 13:40:46 +0700

    feat: add pagination on supplies section

--- src/lib/pages/nft-collection-details/components/CollectionSupplies.tsx
+++ src/lib/pages/nft-collection-details/components/CollectionSupplies.tsx
@@ -67,7 +67,7 @@ export const CollectionSupplies = ({
         }
         showCollection={false}
       />
-      {!isLoading && nfts && (
+      {!isLoading && nfts && nfts.length > 10 && (
         <Pagination
           currentPage={currentPage}
           pagesQuantity={pagesQuantity}
