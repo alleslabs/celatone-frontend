1b75a3dcb5d0ec3a8bf08170909ce58f53fa21dd
Poafs1 <prach@alleslabs.com>
2024-07-08 13:08:13 +0700

    feat(pages): add txs in account details sequencer

--- src/lib/pages/account-details/components/tables/txs/index.tsx
+++ src/lib/pages/account-details/components/tables/txs/index.tsx
@@ -2,11 +2,13 @@ import { TierSwitcher } from "lib/components/TierSwitcher";
 
 import { TxsTableFull } from "./Full";
 import { TxsTableLite } from "./Lite";
+import { TxsTableSequencer } from "./Sequencer";
 import type { TxsTableProps } from "./types";
 
 export const TxsTable = (props: TxsTableProps) => (
   <TierSwitcher
     full={<TxsTableFull {...props} />}
     lite={<TxsTableLite {...props} />}
+    sequencer={<TxsTableSequencer {...props} />}
   />
 );
