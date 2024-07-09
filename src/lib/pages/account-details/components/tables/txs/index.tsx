9ecafad6f3eb30cdf862881732f4bffb1d4a208e
evilpeach <kanisorn.peach@gmail.com>
2024-07-09 16:18:35 +0700

    fix: as comments

--- src/lib/pages/account-details/components/tables/txs/index.tsx
+++ src/lib/pages/account-details/components/tables/txs/index.tsx
@@ -1,14 +1,14 @@
 import { TierSwitcher } from "lib/components/TierSwitcher";
 
-import { TxsTableFull } from "./Full";
-import { TxsTableLite } from "./Lite";
-import { TxsTableSequencer } from "./Sequencer";
+import { TxsTableFull } from "./full";
+import { TxsTableLite } from "./lite";
+import { TxsTableSequencer } from "./sequencer";
 import type { TxsTableProps } from "./types";
 
 export const TxsTable = (props: TxsTableProps) => (
   <TierSwitcher
     full={<TxsTableFull {...props} />}
-    lite={<TxsTableLite {...props} />}
     sequencer={<TxsTableSequencer {...props} />}
+    lite={<TxsTableLite {...props} />}
   />
 );
