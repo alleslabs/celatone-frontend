6c3f4be55dad1af1345e74c853117d1b563536d3
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-07-23 18:04:38 +0700

    fix: props

--- src/lib/components/forms/asset-input/AssetInputFormatOptionLabel.tsx
+++ src/lib/components/forms/asset-input/AssetInputFormatOptionLabel.tsx
@@ -4,20 +4,20 @@ import { TokenImageRenderWithCache } from "lib/components/token";
 import type { AssetOption } from "lib/types";
 import { getTokenLabel } from "lib/utils";
 
-export const AssetInputFormatOptionLabel = (data: AssetOption) => {
-  const { logo, label, id } = data;
-
-  return (
-    <Flex gap={2} alignItems="center">
-      <TokenImageRenderWithCache
-        src={String(logo)}
-        alt={getTokenLabel(String(id), label)}
-        width={24}
-        height={24}
-      />
-      <Text variant="body2" color="text.main">
-        {label}
-      </Text>
-    </Flex>
-  );
-};
+export const AssetInputFormatOptionLabel = ({
+  logo,
+  label,
+  id,
+}: AssetOption) => (
+  <Flex gap={2} alignItems="center">
+    <TokenImageRenderWithCache
+      src={String(logo)}
+      alt={getTokenLabel(String(id), label)}
+      width={24}
+      height={24}
+    />
+    <Text variant="body2" color="text.main">
+      {label}
+    </Text>
+  </Flex>
+);
--- src/lib/components/forms/asset-input/AssetInputNoOptionsMessage.tsx
+++ src/lib/components/forms/asset-input/AssetInputNoOptionsMessage.tsx
@@ -4,10 +4,8 @@ import { components } from "chakra-react-select";
 
 import type { AssetOption } from "lib/types";
 
-export const AssetInputNoOptionsMessage = (props: NoticeProps<AssetOption>) => {
-  return (
-    <components.NoOptionsMessage {...props}>
-      <Text py={4}>No matching assets found</Text>
-    </components.NoOptionsMessage>
-  );
-};
+export const AssetInputNoOptionsMessage = (props: NoticeProps<AssetOption>) => (
+  <components.NoOptionsMessage {...props}>
+    <Text py={4}>No matching assets found</Text>
+  </components.NoOptionsMessage>
+);
