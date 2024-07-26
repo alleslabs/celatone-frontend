194775f638d3eb561c89d95f6c217c6c472e93b0
evilpeach <kanisorn.peach@gmail.com>
2024-07-26 11:41:15 +0700

    feat: support getCollectionByAddress in lite tier

---
+++ src/lib/services/nft/lite.ts
@@ -0,0 +1,57 @@
+/* eslint-disable sonarjs/no-duplicate-string */
+import { z } from "zod";
+
+import { getMoveViewJson } from "../move/module/api";
+import type { CollectionByCollectionAddressResponse } from "../types";
+import { zHexAddr } from "lib/types";
+import type { HexAddr, HexAddr32 } from "lib/types";
+import { parseWithError } from "lib/utils";
+
+export const getCollectionByCollectionAddressLite = async (
+  endpoint: string,
+  collectionAddress: HexAddr32
+) =>
+  Promise.all([
+    getMoveViewJson(
+      endpoint,
+      "0x1" as HexAddr,
+      "collection",
+      "name",
+      ["0x1::collection::Collection"],
+      [`"${collectionAddress}"`]
+    ).then((name) => parseWithError(z.string(), name)),
+    getMoveViewJson(
+      endpoint,
+      "0x1" as HexAddr,
+      "collection",
+      "description",
+      ["0x1::collection::Collection"],
+      [`"${collectionAddress}"`]
+    ).then((name) => parseWithError(z.string(), name)),
+    getMoveViewJson(
+      endpoint,
+      "0x1" as HexAddr,
+      "collection",
+      "uri",
+      ["0x1::collection::Collection"],
+      [`"${collectionAddress}"`]
+    ).then((name) => parseWithError(z.string(), name)),
+    getMoveViewJson(
+      endpoint,
+      "0x1" as HexAddr,
+      "collection",
+      "creator",
+      ["0x1::collection::Collection"],
+      [`"${collectionAddress}"`]
+    ).then((name) => parseWithError(zHexAddr, name)),
+  ]).then<CollectionByCollectionAddressResponse>(
+    ([name, description, uri, creatorAddress]) => ({
+      data: {
+        name,
+        description,
+        uri,
+        createdHeight: null,
+        creatorAddress,
+      },
+    })
+  );
