44379f7f9acf36fca30f3abc1020473951aab8a5
evilpeach <kanisorn.peach@gmail.com>
2024-08-22 15:28:06 +0700

    fix: use parseWithError

--- src/lib/services/wasm/code/api.ts
+++ src/lib/services/wasm/code/api.ts
@@ -36,7 +36,7 @@ export const getCodesByAddress = async (
         offset,
       },
     })
-    .then(({ data }) => zCodesResponse.parse(data));
+    .then(({ data }) => parseWithError(zCodesResponse, data));
 
 export const getAllCodesByAddress = async (
   endpoint: string,
@@ -44,7 +44,7 @@ export const getAllCodesByAddress = async (
 ): Promise<CodesResponse> =>
   axios
     .get(`${endpoint}/${encodeURIComponent(address)}/wasm/all-codes`)
-    .then(({ data }) => zCodesResponse.parse(data));
+    .then(({ data }) => parseWithError(zCodesResponse, data));
 
 export const getCodeData = async (
   endpoint: string,
