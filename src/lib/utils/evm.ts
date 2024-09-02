4810d2e152c9f0e3c232706b4f4a55db2d810e95
songwongtp <16089160+songwongtp@users.noreply.github.com>
2024-09-02 12:17:33 +0900

    fix: msg create method

--- src/lib/utils/evm.ts
+++ src/lib/utils/evm.ts
@@ -1,10 +1,12 @@
 enum EvmMethod {
   transfer = "0x",
   transferErc20 = "0xa9059cbb",
+  create = "0x60806040",
 }
 
 export const getEvmMethod = (txInput: string) => {
   if (txInput === EvmMethod.transfer) return "transfer";
   if (txInput.startsWith(EvmMethod.transferErc20)) return "transfer ERC20";
+  if (txInput.startsWith(EvmMethod.create)) return "create";
   return txInput.slice(0, 10);
 };
