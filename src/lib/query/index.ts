d7e8b0bdce1577f131716cbb813b21bc062a3e5a
Poafs1 <prach@alleslabs.com>
2024-09-06 16:51:25 +0700

    feat(utils): remove graphql query from fe (tx)

--- src/lib/query/index.ts
+++ src/lib/query/index.ts
@@ -2,7 +2,6 @@ export * from "./block";
 export * from "./code";
 export * from "./contract";
 export * from "./pool";
-export * from "./tx";
 export * from "./account";
 export * from "./collection";
 export * from "./collectionOld";
