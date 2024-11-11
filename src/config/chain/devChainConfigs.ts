b66b117d131e97c7147965c3b8a5c2db4d727934
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-11-11 12:52:27 +0400

    add bostrom

--- src/config/chain/devChainConfigs.ts
+++ src/config/chain/devChainConfigs.ts
@@ -5,20 +5,21 @@ export const devChainConfigs: ChainConfig[] = [
   // Write your chain config here.
   {
     tier: "lite",
-    chainId: "localinitia",
-    chain: "localinitia",
-    registryChainName: "localinitia",
-    prettyName: "Local Initia",
-    lcd: "http://localhost:1317",
-    rpc: "http://localhost:26657",
-    wallets: ["initia"],
+    chainId: "bostrom",
+    chain: "bostrom",
+    registryChainName: "bostrom",
+    prettyName: "Bostrom",
+    lcd: "https://lcd.bostrom.cybernode.ai:443",
+    rpc: "https://rpc.bostrom.cybernode.ai:443",
+    wallets: ["keplr"],
     features: {
       wasm: {
-        enabled: false,
+        enabled: true,
+        storeCodeMaxFileSize: 1_048_576,
+        clearAdminGas: 1_048_576,
       },
       move: {
-        enabled: true,
-        moduleMaxFileSize: 1_048_576,
+        enabled: false,
       },
       evm: {
         enabled: false,
@@ -40,12 +41,12 @@ export const devChainConfigs: ChainConfig[] = [
     },
     gas: {
       gasAdjustment: 1.5,
-      maxGasLimit: 200_000_000,
+      maxGasLimit: 20_000_000,
     },
     extra: {
       layer: "1",
     },
-    network_type: "testnet",
+    network_type: "mainnet",
     logo_URIs: {
       png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.png",
       svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.svg",
@@ -53,41 +54,37 @@ export const devChainConfigs: ChainConfig[] = [
     fees: {
       fee_tokens: [
         {
-          denom: "uinit",
-          fixed_min_gas_price: 0.15,
-          low_gas_price: 0.15,
-          average_gas_price: 0.15,
-          high_gas_price: 0.4,
+          denom: "boot",
+          fixed_min_gas_price: 0.2,
+          low_gas_price: 0.2,
+          average_gas_price: 0.2,
+          high_gas_price: 0.5,
         },
       ],
     },
     registry: {
-      bech32_prefix: "init",
+      bech32_prefix: "bostrom",
       slip44: 118,
       staking: {
         staking_tokens: [
           {
-            denom: "uinit",
+            denom: "boot",
           },
         ],
       },
       assets: [
         {
-          description: "The native token of Initia",
+          description: "The native token of Bostrom",
           denom_units: [
             {
-              denom: "uinit",
+              denom: "boot",
               exponent: 0,
             },
-            {
-              denom: "INIT",
-              exponent: 6,
-            },
           ],
-          base: "uinit",
-          display: "INIT",
-          name: "Initia Native Token",
-          symbol: "INIT",
+          base: "boot",
+          display: "BOOT",
+          name: "BOSTROM Native Token",
+          symbol: "BOOT",
           images: [
             {
               png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.png",
@@ -102,4 +99,103 @@ export const devChainConfigs: ChainConfig[] = [
       ],
     },
   },
+  // {
+  //   tier: "lite",
+  //   chainId: "localinitia",
+  //   chain: "localinitia",
+  //   registryChainName: "localinitia",
+  //   prettyName: "Local Initia",
+  //   lcd: "http://localhost:1317",
+  //   rpc: "http://localhost:26657",
+  //   wallets: ["initia"],
+  //   features: {
+  //     wasm: {
+  //       enabled: false,
+  //     },
+  //     move: {
+  //       enabled: true,
+  //       moduleMaxFileSize: 1_048_576,
+  //     },
+  //     evm: {
+  //       enabled: false,
+  //     },
+  //     pool: {
+  //       enabled: false,
+  //     },
+  //     publicProject: {
+  //       enabled: false,
+  //     },
+  //     gov: {
+  //       enabled: true,
+  //       version: "v1",
+  //       hideOpenProposal: true,
+  //     },
+  //     nft: {
+  //       enabled: false,
+  //     },
+  //   },
+  //   gas: {
+  //     gasAdjustment: 1.5,
+  //     maxGasLimit: 200_000_000,
+  //   },
+  //   extra: {
+  //     layer: "1",
+  //   },
+  //   network_type: "testnet",
+  //   logo_URIs: {
+  //     png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.png",
+  //     svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.svg",
+  //   },
+  //   fees: {
+  //     fee_tokens: [
+  //       {
+  //         denom: "uinit",
+  //         fixed_min_gas_price: 0.15,
+  //         low_gas_price: 0.15,
+  //         average_gas_price: 0.15,
+  //         high_gas_price: 0.4,
+  //       },
+  //     ],
+  //   },
+  //   registry: {
+  //     bech32_prefix: "init",
+  //     slip44: 118,
+  //     staking: {
+  //       staking_tokens: [
+  //         {
+  //           denom: "uinit",
+  //         },
+  //       ],
+  //     },
+  //     assets: [
+  //       {
+  //         description: "The native token of Initia",
+  //         denom_units: [
+  //           {
+  //             denom: "uinit",
+  //             exponent: 0,
+  //           },
+  //           {
+  //             denom: "INIT",
+  //             exponent: 6,
+  //           },
+  //         ],
+  //         base: "uinit",
+  //         display: "INIT",
+  //         name: "Initia Native Token",
+  //         symbol: "INIT",
+  //         images: [
+  //           {
+  //             png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.png",
+  //             svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.svg",
+  //           },
+  //         ],
+  //         logo_URIs: {
+  //           png: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.png",
+  //           svg: "https://raw.githubusercontent.com/initia-labs/initia-registry/main/testnets/initia/images/INIT.svg",
+  //         },
+  //       },
+  //     ],
+  //   },
+  // },
 ];
