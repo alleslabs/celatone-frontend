b894991596209276074f92600de08d6f2f719ec4
evilpeach <kanisorn.peach@gmail.com>
2025-01-08 15:30:52 +0700

    feat: add custom layer on Initia widget

--- src/lib/providers/initia-widget.tsx
+++ src/lib/providers/initia-widget.tsx
@@ -1,6 +1,32 @@
 import { WalletWidgetProvider } from "@initia/react-wallet-widget";
 import type { ReactNode } from "react";
 
-export const InitiaWidgetProvider = ({ children }: { children: ReactNode }) => (
-  <WalletWidgetProvider>{children}</WalletWidgetProvider>
-);
+import { useCelatoneApp } from "lib/app-provider";
+
+export const InitiaWidgetProvider = ({ children }: { children: ReactNode }) => {
+  const { chainConfig, currentChainId } = useCelatoneApp();
+
+  return (
+    <WalletWidgetProvider
+      customLayer={
+        chainConfig.network_type === "local"
+          ? {
+              apis: {
+                rest: [{ address: chainConfig.lcd }],
+                rpc: [{ address: chainConfig.rpc }],
+              },
+              bech32_prefix: "init" as const,
+              chain_id: currentChainId,
+              chain_name: chainConfig.chain,
+              fees: chainConfig.fees ?? { fee_tokens: [] },
+              logo_URIs: chainConfig.logo_URIs,
+              network_type: "devnet",
+              pretty_name: chainConfig.prettyName,
+            }
+          : undefined
+      }
+    >
+      {children}
+    </WalletWidgetProvider>
+  );
+};
