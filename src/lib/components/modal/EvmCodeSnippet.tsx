a86cd40d45424b7b7858955aea2e8e81cb16e2da
evilpeach <kanisorn.peach@gmail.com>
2025-02-04 15:37:51 +0700

    fix: disable evm code snippet

--- src/lib/components/modal/EvmCodeSnippet.tsx
+++ src/lib/components/modal/EvmCodeSnippet.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { ButtonProps } from "@chakra-ui/react";
 import {
   Button,
@@ -27,54 +28,57 @@ interface EvmCodeSnippetProps {
   ml?: ButtonProps["ml"];
 }
 
+// TODO: Implement this modal
 const EvmCodeSnippet = ({ ml }: EvmCodeSnippetProps) => {
   const { isOpen, onClose, onOpen } = useDisclosure();
 
-  return (
-    <>
-      <Button
-        variant="outline-secondary"
-        minW="128px"
-        size="sm"
-        ml={ml}
-        gap={1}
-        onClick={() => {
-          track(AmpEvent.USE_CONTRACT_SNIPPET, {});
-          onOpen();
-        }}
-      >
-        <CustomIcon name="code" />
-        Code Snippet
-      </Button>
+  return null;
 
-      <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
-        <ModalOverlay />
-        <ModalContent w="840px">
-          <ModalHeader>
-            <CustomIcon name="code" boxSize={6} color="gray.600" />
-            <Heading as="h5" variant="h5">
-              Code Snippet
-            </Heading>
-          </ModalHeader>
-          <ModalCloseButton color="gray.600" />
-          <ModalBody px={4} maxH="640px" overflow="scroll">
-            <Text wordBreak="break-word">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book. It has
-              survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was
-              popularised in the 1960s with the release of Letraset sheets
-              containing Lorem Ipsum passages, and more recently with desktop
-              publishing software like Aldus PageMaker including versions of
-              Lorem Ipsum.
-            </Text>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
-    </>
-  );
+  // return (
+  //   <>
+  //     <Button
+  //       variant="outline-secondary"
+  //       minW="128px"
+  //       size="sm"
+  //       ml={ml}
+  //       gap={1}
+  //       onClick={() => {
+  //         track(AmpEvent.USE_CONTRACT_SNIPPET, {});
+  //         onOpen();
+  //       }}
+  //     >
+  //       <CustomIcon name="code" />
+  //       Code Snippet
+  //     </Button>
+
+  //     <Modal isOpen={isOpen} onClose={onClose} isCentered size="4xl">
+  //       <ModalOverlay />
+  //       <ModalContent w="840px">
+  //         <ModalHeader>
+  //           <CustomIcon name="code" boxSize={6} color="gray.600" />
+  //           <Heading as="h5" variant="h5">
+  //             Code Snippet
+  //           </Heading>
+  //         </ModalHeader>
+  //         <ModalCloseButton color="gray.600" />
+  //         <ModalBody px={4} maxH="640px" overflow="scroll">
+  //           <Text wordBreak="break-word">
+  //             Lorem Ipsum is simply dummy text of the printing and typesetting
+  //             industry. Lorem Ipsum has been the industry's standard dummy text
+  //             ever since the 1500s, when an unknown printer took a galley of
+  //             type and scrambled it to make a type specimen book. It has
+  //             survived not only five centuries, but also the leap into
+  //             electronic typesetting, remaining essentially unchanged. It was
+  //             popularised in the 1960s with the release of Letraset sheets
+  //             containing Lorem Ipsum passages, and more recently with desktop
+  //             publishing software like Aldus PageMaker including versions of
+  //             Lorem Ipsum.
+  //           </Text>
+  //         </ModalBody>
+  //       </ModalContent>
+  //     </Modal>
+  //   </>
+  // );
 };
 
 export default EvmCodeSnippet;
