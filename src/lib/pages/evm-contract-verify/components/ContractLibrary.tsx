dc1dc5096f3ef223b9fd3bd209db4d3ad86ea788
songwongtp <16089160+songwongtp@users.noreply.github.com>
2025-02-11 00:22:33 +0700

    feat: validate evm verify form options

---
+++ src/lib/pages/evm-contract-verify/components/ContractLibrary.tsx
@@ -0,0 +1,56 @@
+import { useExampleAddresses } from "lib/app-provider";
+import { ControllerInput } from "lib/components/forms";
+import { truncate } from "lib/utils";
+import {
+  Control,
+  FieldPath,
+  FieldValues,
+  useController,
+} from "react-hook-form";
+
+interface ContractLibraryProps<T extends FieldValues> {
+  control: Control<T>;
+  name: FieldPath<T>;
+}
+
+export const ContractLibrary = <T extends FieldValues>({
+  control,
+  name,
+}: ContractLibraryProps<T>) => {
+  const { evmContract: exampleContractAddress } = useExampleAddresses();
+  const { field } = useController({
+    control,
+    name,
+  });
+
+  return (
+    <>
+      <ControllerInput
+        label="Library Name"
+        isRequired
+        rules={{
+          required: "",
+        }}
+        placeholder="ex. simple_math"
+        name={`${name}.name` as FieldPath<T>}
+        control={control}
+        variant="fixed-floating"
+        labelBgColor="gray.900"
+        error={field.value.name !== "" ? "" : "Required"}
+      />
+      <ControllerInput
+        label="Contract Library Address"
+        isRequired
+        rules={{
+          required: "",
+        }}
+        placeholder={`ex. ${truncate(exampleContractAddress)}`}
+        name={`${name}.address` as FieldPath<T>}
+        control={control}
+        variant="fixed-floating"
+        labelBgColor="gray.900"
+        error={field.value.address !== "" ? "" : "Required"}
+      />
+    </>
+  );
+};
