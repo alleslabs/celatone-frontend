d4f7830c41fe4668fce3dab331a955fd67c9a90f
evilpeach <kanisorn.peach@gmail.com>
2025-02-28 13:45:55 +0700

    feat: remove harded-code lcd in username

--- src/lib/services/username/lcd.ts
+++ src/lib/services/username/lcd.ts
@@ -1,16 +1,13 @@
+import { INITIA_USERNAME_LCD, INITIA_USERNAME_MODULE_ADDRESS } from "env";
 import type { HexAddr, HexAddr32 } from "lib/types";
 import { zHexAddr } from "lib/types";
 import { parseWithError } from "lib/utils";
 import { getMoveViewJsonLcd } from "../move/module/lcd";
 
-const INITIATION_2_LCD = "https://rest.initiation-2.initia.xyz";
-const USERNAME_MODULE_ADDRESS =
-  "0x42cd8467b1c86e59bf319e5664a09b6b5840bb3fac64f5ce690b5041c530565a" as HexAddr32;
-
 export const getInitiaUsernameByAddress = async (address: HexAddr) =>
   getMoveViewJsonLcd(
-    INITIATION_2_LCD,
-    USERNAME_MODULE_ADDRESS,
+    INITIA_USERNAME_LCD,
+    INITIA_USERNAME_MODULE_ADDRESS as HexAddr32,
     "usernames",
     "get_name_from_address",
     [],
@@ -19,8 +16,8 @@ export const getInitiaUsernameByAddress = async (address: HexAddr) =>
 
 export const getAddressByInitiaUsername = async (username: string) =>
   getMoveViewJsonLcd(
-    INITIATION_2_LCD,
-    USERNAME_MODULE_ADDRESS,
+    INITIA_USERNAME_LCD,
+    INITIA_USERNAME_MODULE_ADDRESS as HexAddr32,
     "usernames",
     "get_address_from_name",
     [],
