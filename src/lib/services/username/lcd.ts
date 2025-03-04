0d963f5aa91e4c602119f56b311c0712d3150944
evilpeach <kanisorn.peach@gmail.com>
2025-03-04 12:51:00 +0700

    fix: use zHexAddr32 parse

--- src/lib/services/username/lcd.ts
+++ src/lib/services/username/lcd.ts
@@ -1,13 +1,13 @@
 import { INITIA_USERNAME_LCD, INITIA_USERNAME_MODULE_ADDRESS } from "env";
-import type { HexAddr, HexAddr32 } from "lib/types";
-import { zHexAddr } from "lib/types";
+import type { HexAddr } from "lib/types";
+import { zHexAddr, zHexAddr32 } from "lib/types";
 import { parseWithError } from "lib/utils";
 import { getMoveViewJsonLcd } from "../move/module/lcd";
 
 export const getInitiaUsernameByAddress = async (address: HexAddr) =>
   getMoveViewJsonLcd(
     INITIA_USERNAME_LCD,
-    INITIA_USERNAME_MODULE_ADDRESS as HexAddr32,
+    zHexAddr32.parse(INITIA_USERNAME_MODULE_ADDRESS),
     "usernames",
     "get_name_from_address",
     [],
@@ -17,7 +17,7 @@ export const getInitiaUsernameByAddress = async (address: HexAddr) =>
 export const getAddressByInitiaUsername = async (username: string) =>
   getMoveViewJsonLcd(
     INITIA_USERNAME_LCD,
-    INITIA_USERNAME_MODULE_ADDRESS as HexAddr32,
+    zHexAddr32.parse(INITIA_USERNAME_MODULE_ADDRESS),
     "usernames",
     "get_address_from_name",
     [],
