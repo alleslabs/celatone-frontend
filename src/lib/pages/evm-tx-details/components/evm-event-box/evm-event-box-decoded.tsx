a0e95b0b3d2f930341bbfca4ab341d09da390c19
Prach Yot <prach@Prachs-MacBook-Air.local>
2025-03-04 16:18:27 +0700

    feat(components): full address

--- src/lib/pages/evm-tx-details/components/evm-event-box/evm-event-box-decoded.tsx
+++ src/lib/pages/evm-tx-details/components/evm-event-box/evm-event-box-decoded.tsx
@@ -24,6 +24,7 @@ const EvmEventBoxDecodedBody = ({
           value={decode.toString()}
           type="user_address"
           openNewTab
+          textFormat="normal"
         />
       );
     case "tuple":
