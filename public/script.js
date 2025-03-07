028eff30efbf5b10d104278b19a56c9b3eeb0694
songwongtp <16089160+songwongtp@users.noreply.github.com>
2025-03-07 12:23:00 +0700

    fix: vercel name

--- public/script.js
+++ public/script.js
@@ -289,7 +289,7 @@ const INITIA_TERMS = [
 
 function getCurrentDomain() {
   if (location.hostname.endsWith(INITIA_DOMAIN)) return INITIA_DOMAIN;
-  if (location.hostname.endsWith(".vercel.live")) return ".vercel.live";
+  if (location.hostname.endsWith(".vercel.app")) return ".vercel.app";
   return location.hostname;
 }
 
