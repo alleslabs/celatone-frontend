e10ab2fa16e8b6ab1604dc6ed25763be351efcec
songwongtp <16089160+songwongtp@users.noreply.github.com>
2025-03-12 19:12:29 +0700

    feat: bump wallet 1.0.0

--- src/lib/providers/initia-widget.tsx
+++ src/lib/providers/initia-widget.tsx
@@ -1,8 +1,49 @@
-import { WalletWidgetProvider } from "@initia/react-wallet-widget";
-import type { ReactNode } from "react";
+import { context, loadScript } from "@initia/react-wallet-widget/ssr";
+import type { WalletWidget, WidgetConfig, WidgetWallet } from "@initia/utils";
+import { useEffect, useState } from "react";
+import type { ReactNode, PropsWithChildren } from "react";
 
 import { useCelatoneApp, useWasmConfig } from "lib/app-provider";
 import { LoadingOverlay } from "lib/components/LoadingOverlay";
+
+declare global {
+  interface Window {
+    createWalletWidget?: (config: WidgetConfig) => Promise<WalletWidget>;
+  }
+}
+
+// You can specify the version of wallet-widget you want to use here.
+// While you can use "latest", we recommend explicitly specifying the latest version number and updating it manually when needed for better stability.
+// (You can check the latest available version here: https://www.npmjs.com/package/@initia/wallet-widget)
+const VERSION = "1.0.0";
+
+const WalletWidgetProvider = ({
+  children,
+  fallback = null,
+  ...config
+}: PropsWithChildren<WidgetConfig> & {
+  fallback?: ReactNode;
+}) => {
+  const [widget, setWidget] = useState<WalletWidget | null>(null);
+
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+
+    async function setup() {
+      await loadScript(
+        `https://cdn.jsdelivr.net/npm/@initia/wallet-widget@${VERSION}/dist/index.js`
+      );
+      const widget = await window.createWalletWidget!(config);
+      setWidget(widget);
+    }
+
+    setup();
+  }, [config]);
+
+  if (!widget) return fallback;
+
+  return <context.Provider value={widget}>{children}</context.Provider>;
+};
 
 export const InitiaWidgetProvider = ({ children }: { children: ReactNode }) => {
   const { chainConfig, currentChainId } = useCelatoneApp();
@@ -22,6 +63,8 @@ export const InitiaWidgetProvider = ({ children }: { children: ReactNode }) => {
       swap_transfer:
         "0x42cd8467b1c86e59bf319e5664a09b6b5840bb3fac64f5ce690b5041c530565a",
     },
+    filterWallet: (wallet: WidgetWallet) =>
+      !chainConfig.features.evm.enabled || wallet.type === "evm",
   };
 
   return (
@@ -51,10 +94,6 @@ export const InitiaWidgetProvider = ({ children }: { children: ReactNode }) => {
           : undefined
       }
       fallback={<LoadingOverlay />}
-      filterWallet={(wallet) =>
-        wallet.type !== "initia" &&
-        (!chainConfig.features.evm.enabled || wallet.type === "evm")
-      }
       {...(chainConfig.network_type === "testnet" && testnetConfigs)}
     >
       {children}
