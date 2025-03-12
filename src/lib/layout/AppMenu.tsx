d4fda58ba7b968e8495a14fdc6486ca54fc4cd96
Prach Yot <prach@Prachs-MacBook-Air.local>
2025-03-12 13:24:20 +0700

    feat: remove multisig form app menu

--- src/lib/layout/AppMenu.tsx
+++ src/lib/layout/AppMenu.tsx
@@ -33,11 +33,6 @@ const appList = [
     logo: "https://assets.alleslabs.dev/integrations/initia/app-logo/bridge.svg",
     link: getAppLink("bridge"),
   },
-  {
-    name: "multisig",
-    logo: "https://assets.alleslabs.dev/integrations/initia/app-logo/multisig.svg",
-    link: getAppLink("multisig"),
-  },
   SUPPORTED_NETWORK_TYPES[0] === "mainnet"
     ? null
     : {
