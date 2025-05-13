fea7b9198638cc2da4e6ae4f794d31c19f1bdae7
Poafs1 <prach.yot@gmail.com>
2025-05-13 13:28:21 +0700

    feat(pages): support contract txs from indexer

---
+++ src/lib/pages/contract-details/components/tables/txs/full.tsx
@@ -0,0 +1,81 @@
+import { Pagination } from "lib/components/pagination";
+import { usePaginator } from "lib/components/pagination/usePaginator";
+import { EmptyState, ErrorFetching } from "lib/components/state";
+import { TransactionsTable } from "lib/components/table";
+import { DEFAULT_TX_FILTERS } from "lib/data";
+import { useTxsByAddress } from "lib/services/tx";
+
+import type { TxsTableProps } from "./types";
+
+export const TxsTableFull = ({
+  contractAddress,
+  refetchCount,
+  scrollComponentId,
+  totalData,
+}: TxsTableProps) => {
+  const {
+    currentPage,
+    offset,
+    pageSize,
+    pagesQuantity,
+    setCurrentPage,
+    setPageSize,
+  } = usePaginator({
+    initialState: {
+      currentPage: 1,
+      isDisabled: false,
+      pageSize: 10,
+    },
+    total: totalData,
+  });
+
+  const { data, error, isLoading } = useTxsByAddress(
+    contractAddress,
+    undefined,
+    undefined,
+    DEFAULT_TX_FILTERS,
+    pageSize,
+    offset
+  );
+
+  return (
+    <>
+      <TransactionsTable
+        emptyState={
+          error ? (
+            <ErrorFetching dataName="transactions" />
+          ) : (
+            <EmptyState
+              imageVariant="empty"
+              message="This contract does not have any transactions."
+              withBorder
+            />
+          )
+        }
+        isLoading={isLoading}
+        showRelations={false}
+        transactions={data?.items}
+      />
+      {!!totalData && totalData > 10 && (
+        <Pagination
+          currentPage={currentPage}
+          offset={offset}
+          pageSize={pageSize}
+          pagesQuantity={pagesQuantity}
+          scrollComponentId={scrollComponentId}
+          totalData={totalData}
+          onPageChange={(nextPage) => {
+            setCurrentPage(nextPage);
+            refetchCount();
+          }}
+          onPageSizeChange={(e) => {
+            const size = Number(e.target.value);
+            setPageSize(size);
+            setCurrentPage(1);
+            refetchCount();
+          }}
+        />
+      )}
+    </>
+  );
+};
