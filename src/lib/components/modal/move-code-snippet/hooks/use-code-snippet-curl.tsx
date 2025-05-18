97e8ebae44f94ec987afe2c950b3fb837d8d8070
Poafs1 <prach.yot@gmail.com>
2025-05-18 12:13:14 +0700

    feat(utils): update code snippet curl

--- src/lib/components/modal/move-code-snippet/hooks/use-code-snippet-curl.tsx
+++ src/lib/components/modal/move-code-snippet/hooks/use-code-snippet-curl.tsx
@@ -15,8 +15,21 @@ export const useSnippetCurl = ({
 
   const serializedAbiDataJson = serializeAbiDataJson(fn, abiData);
 
-  const viewSnippet = `\n\ncurl '${restEndpoint}/initia/move/v1/accounts/${moduleAddress}/modules/${moduleName}/view_functions/${fn.name}' \\
-    --data-raw '${JSON.stringify(serializedAbiDataJson)}'`;
+  const showTypeArgsJson = serializedAbiDataJson.typeArgs.length > 0;
+  const formatedTypeArgsJson = JSON.stringify(serializedAbiDataJson.typeArgs);
+
+  const showArgsJson = serializedAbiDataJson.args.length > 0;
+  const formatedArgsJson = JSON.stringify(serializedAbiDataJson.args);
+
+  const viewSnippet = `\n\ncurl --location '${restEndpoint}/initia/move/v1/view/json' \\
+--header 'Content-Type: application/json' \\
+--data '{
+    "address": "${moduleAddress}",
+    "args": ${showArgsJson ? formatedArgsJson : "[]"},
+    "function_name": "${fn.name}",
+    "module_name": "${moduleName}",
+    "typeArgs": ${showTypeArgsJson ? formatedTypeArgsJson : "[]"}
+}'`;
 
   return {
     mode: "sh",
