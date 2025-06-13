702f15bce6d0ee42e3785912a4f091324805ab6b
evilpeach <kanisorn.peach@gmail.com>
2025-06-13 10:02:29 +0700

    feat: add sitemap, robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,14 @@
+const isMainnet = process.env.NEXT_PUBLIC_SUPPORTED_NETWORK_TYPES === "mainnet";
+
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
   generateRobotsTxt: true,
-  siteUrl: "",
+  robotsTxtOptions: {
+    policies: isMainnet
+      ? [{ allow: "/", userAgent: "*" }]
+      : [{ disallow: "/", userAgent: "Googlebot" }],
+  },
+  siteUrl: "https://scan.initia.xyz",
 };
 
 module.exports = NextSitemapConfig;
