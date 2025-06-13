bc9baeb7c8985c1130fbe581ef0c80416b6a7308
evilpeach <kanisorn.peach@gmail.com>
2025-06-13 11:35:44 +0700

    feat: configure sitemap and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,14 @@
+const isMainnet = process.env.NEXT_PUBLIC_SUPPORTED_NETWORK_TYPES === "mainnet";
+
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
   generateRobotsTxt: true,
-  siteUrl: "",
+  robotsTxtOptions: {
+    policies: isMainnet
+      ? [{ allow: "/", userAgent: "*" }]
+      : [{ disallow: "/", userAgent: "Googlebot" }],
+  },
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
 };
 
 module.exports = NextSitemapConfig;
