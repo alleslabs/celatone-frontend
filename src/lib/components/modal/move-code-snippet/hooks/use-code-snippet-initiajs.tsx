1a905360c6f15a04a9d8ead5fb68a5dcefd04213
songwongtp <16089160+songwongtp@users.noreply.github.com>
2025-06-30 13:34:58 +0700

    fix: indent

--- src/lib/components/modal/move-code-snippet/hooks/use-code-snippet-initiajs.tsx
+++ src/lib/components/modal/move-code-snippet/hooks/use-code-snippet-initiajs.tsx
@@ -26,59 +26,59 @@ export const useCodeSnippetInitiaJs = ({
   const formatedArgsJson = JSON.stringify(serializedAbiDataJson.args);
 
   const executeCodeSnippet = `import {
-    RESTClient,
-    Wallet,
-    MnemonicKey,
-    MsgExecuteJSON,
-  } from '@initia/initia.js';
+  RESTClient,
+  Wallet,
+  MnemonicKey,
+  MsgExecuteJSON,
+} from '@initia/initia.js';
 
-  const client = new RESTClient('${restEndpoint}', {
-    chainId: '${currentChainId}',
-    gasPrices: '${gasPriceStr}',
-    gasAdjustment: '2.0',
+const client = new RESTClient('${restEndpoint}', {
+  chainId: '${currentChainId}',
+  gasPrices: '${gasPriceStr}',
+  gasAdjustment: '2.0',
+});
+const key = new MnemonicKey({
+  mnemonic: "<MNEMONIC>",
+});
+const wallet = new Wallet(client, key);
+const msg = new MsgExecuteJSON(
+  key.accAddress,
+  '${moduleAddress}',
+  '${moduleName}',
+  '${fn.name}',
+  ${showTypeArgsJson ? formatedTypeArgsJson : "[]"},
+  ${showArgsJson ? formatedArgsJson : "[]"}
+);
+
+const execute = async () => {
+  const signedTx = await wallet.createAndSignTx({
+      msgs: [msg],
   });
-  const key = new MnemonicKey({
-    mnemonic: "<MNEMONIC>",
-  });
-  const wallet = new Wallet(client, key);
-  const msg = new MsgExecuteJSON(
-    key.accAddress,
-    '${moduleAddress}',
-    '${moduleName}',
-    '${fn.name}',
-    ${showTypeArgsJson ? formatedTypeArgsJson : "[]"},
-    ${showArgsJson ? formatedArgsJson : "[]"}
-  );
-  
-  const execute = async () => {
-    const signedTx = await wallet.createAndSignTx({
-        msgs: [msg],
-    });
-  
-    const broadcastResult = await client.tx.broadcast(signedTx);
-    console.log(broadcastResult);
-  };
-  execute();`;
+
+  const broadcastResult = await client.tx.broadcast(signedTx);
+  console.log(broadcastResult);
+};
+execute();`;
 
   const viewCodeSnippet = `import { RESTClient } from '@initia/initia.js'
-      const client = new RESTClient('${restEndpoint}', {
-          chainId: '${currentChainId}',
-      });
-      const moduleAddress =
-      "${moduleAddress}";
-      const moduleName = "${moduleName}";
-      const fnName = "${fn.name}";
-      const viewModule = async (moduleAddress, moduleName, fnName) => {
-          const viewResult = await client.move.viewJSON(
-              moduleAddress,
-              moduleName,
-              fnName,
-              ${showTypeArgsJson ? formatedTypeArgsJson : "[]"},
-              ${showArgsJson ? formatedArgsJson : "[]"}
-          )
-          console.log(viewResult);
-      };\n
-      viewModule(moduleAddress, moduleName, fnName);`;
+const client = new RESTClient('${restEndpoint}', {
+    chainId: '${currentChainId}',
+});
+const moduleAddress =
+"${moduleAddress}";
+const moduleName = "${moduleName}";
+const fnName = "${fn.name}";
+const viewModule = async (moduleAddress, moduleName, fnName) => {
+    const viewResult = await client.move.viewJSON(
+        moduleAddress,
+        moduleName,
+        fnName,
+        ${showTypeArgsJson ? formatedTypeArgsJson : "[]"},
+        ${showArgsJson ? formatedArgsJson : "[]"}
+    )
+    console.log(viewResult);
+};\n
+viewModule(moduleAddress, moduleName, fnName);`;
 
   return {
     executeCodeSnippet,
