0c2fefbd522453d536dc91661aeafe9609874335
Poafs1 <prach.yot@gmail.com>
2025-07-21 15:51:26 +0700

    feat(components): add evm blocks table

--- src/lib/components/table/evm-blocks/EvmBlocksTable.tsx
+++ src/lib/components/table/evm-blocks/EvmBlocksTable.tsx
@@ -5,54 +5,42 @@ import { Loading } from "lib/components/Loading";
 import { ErrorFetching } from "lib/components/state";
 import { MobileTableContainer, TableContainer } from "lib/components/table";
 
-import { BlocksTableHeader } from "./BlocksTableHeader";
-import { BlocksTableMobileCard } from "./BlocksTableMobileCard";
-import { BlocksTableRow } from "./BlocksTableRow";
+import { EvmBlocksTableHeader } from "./EvmBlocksTableHeader";
+import { EvmBlocksTableMobileCard } from "./EvmBlocksTableMobileCard";
+import { EvmBlocksTableRow } from "./EvmBlocksTableRow";
 
-interface BlocksTableProps {
+interface EvmBlocksTableProps {
   blocks: Option<Block[]>;
   emptyState: JSX.Element;
   isLoading: boolean;
-  showProposer?: boolean;
 }
 
-export const BlocksTable = ({
+export const EvmBlocksTable = ({
   blocks,
   emptyState,
   isLoading,
-  showProposer = true,
-}: BlocksTableProps) => {
+}: EvmBlocksTableProps) => {
   const isMobile = useMobile();
 
   if (isLoading) return <Loading />;
   if (!blocks) return <ErrorFetching dataName="blocks" />;
   if (!blocks.length) return emptyState;
 
-  const templateColumns = showProposer
-    ? "140px 160px minmax(300px,1fr) 120px 280px"
-    : "150px minmax(160px, 1fr) 180px 280px";
+  const templateColumns = "140px 160px minmax(300px,1fr) 120px 120px 280px";
 
   return isMobile ? (
     <MobileTableContainer>
       {blocks.map((block) => (
-        <BlocksTableMobileCard
-          key={block.hash}
-          blockData={block}
-          showProposer={showProposer}
-        />
+        <EvmBlocksTableMobileCard key={block.hash} blockData={block} />
       ))}
     </MobileTableContainer>
   ) : (
     <TableContainer>
-      <BlocksTableHeader
-        showProposer={showProposer}
-        templateColumns={templateColumns}
-      />
+      <EvmBlocksTableHeader templateColumns={templateColumns} />
       {blocks.map((block) => (
-        <BlocksTableRow
+        <EvmBlocksTableRow
           key={block.hash}
           blockData={block}
-          showProposer={showProposer}
           templateColumns={templateColumns}
         />
       ))}
--- src/lib/components/table/evm-blocks/EvmBlocksTableHeader.tsx
+++ src/lib/components/table/evm-blocks/EvmBlocksTableHeader.tsx
@@ -3,23 +3,22 @@ import type { GridProps } from "@chakra-ui/react";
 import { Grid } from "@chakra-ui/react";
 import { TableHeader } from "lib/components/table";
 
-interface BlocksTableHeaderProps {
-  showProposer: boolean;
+interface EvmBlocksTableHeaderProps {
   templateColumns: GridProps["templateColumns"];
 }
 
-export const BlocksTableHeader = ({
-  showProposer = true,
+export const EvmBlocksTableHeader = ({
   templateColumns,
-}: BlocksTableHeaderProps) => (
+}: EvmBlocksTableHeaderProps) => (
   <Grid
     sx={{ "> div": { color: "text.dark" } }}
     templateColumns={templateColumns}
   >
     <TableHeader>Block height</TableHeader>
     <TableHeader>Block hash</TableHeader>
-    {showProposer && <TableHeader>Proposed by</TableHeader>}
-    <TableHeader textAlign="center">Transactions</TableHeader>
+    <TableHeader>Proposed by</TableHeader>
+    <TableHeader textAlign="center">EVM Txs</TableHeader>
+    <TableHeader textAlign="center">Cosmos Txs</TableHeader>
     <TableHeader>Timestamp</TableHeader>
   </Grid>
 );
