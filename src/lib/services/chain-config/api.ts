c2c6fd4934090de467676eb46a820a6bd4ca98ae
evilpeach <kanisorn.peach@gmail.com>
2025-07-22 17:49:07 +0700

    refactor: update API endpoint logic and add URL validation

--- src/lib/services/chain-config/api.ts
+++ src/lib/services/chain-config/api.ts
@@ -2,25 +2,34 @@ import type { Option } from "lib/types";
 import type { ChainProfile } from "lib/types/chainProfile";
 
 import axios from "axios";
-import { CELATONE_API } from "env";
+import { CHAIN, SCAN_API } from "env";
 import { CHAIN_PROFILE_URL } from "lib/data";
 import { zChainConfig } from "lib/types";
 import { zChainProfile } from "lib/types/chainProfile";
-import { parseWithError } from "lib/utils";
+import { isUrl, parseWithError } from "lib/utils";
 
 export const getApiChainConfigs = (
-  endpoint: string,
   networkTypes: string[],
   chain: Option<string>
-) =>
-  axios
-    .get(`${CELATONE_API}/v1/configs`, {
+) => {
+  const endpoint =
+    CHAIN === "initia" ? `${SCAN_API}/v1/chains` : `${SCAN_API}/v1/configs`;
+
+  if (!isUrl(String(endpoint))) {
+    throw new Error(
+      `Endpoint is not a valid URL (getApiChainConfigs): ${endpoint}`
+    );
+  }
+
+  return axios
+    .get(endpoint, {
       params: {
         chain,
         network_types: networkTypes.join(","),
       },
     })
     .then(({ data }) => parseWithError(zChainConfig.array(), data));
+};
 
 export const getChainProfile = () =>
   axios.get(CHAIN_PROFILE_URL).then(({ data }) => {
