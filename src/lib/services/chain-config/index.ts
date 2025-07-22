e58994741825ccb130885f9905142799fbdcc0e4
evilpeach <kanisorn.peach@gmail.com>
2025-07-22 17:49:07 +0700

    refactor: update API endpoint logic and add URL validation

--- src/lib/services/chain-config/index.ts
+++ src/lib/services/chain-config/index.ts
@@ -1,32 +1,25 @@
 import type { Option } from "lib/types";
 
 import { useQuery } from "@tanstack/react-query";
-import { CHAIN, SCAN_API } from "env";
 import { useInitia } from "lib/app-provider";
 import { CELATONE_QUERY_KEYS } from "lib/app-provider/env";
-import { isUrl } from "lib/utils";
 
 import { getApiChainConfigs, getChainProfile } from "./api";
 
 export const useApiChainConfigs = (
   networkTypes: string[],
   chain: Option<string>
-) => {
-  const endpoint =
-    CHAIN === "initia" ? `${SCAN_API}/v1/chains` : `${SCAN_API}/v1/configs`;
-
-  return useQuery(
+) =>
+  useQuery(
     [CELATONE_QUERY_KEYS.CHAIN_CONFIGS, networkTypes, chain],
-    () => getApiChainConfigs(endpoint, networkTypes, chain),
+    () => getApiChainConfigs(networkTypes, chain),
     {
-      enabled: isUrl(String(SCAN_API)),
       refetchOnMount: false,
       refetchOnWindowFocus: false,
       retry: 1,
       staleTime: Infinity,
     }
   );
-};
 
 export const useChainProfile = () => {
   const isInitia = useInitia();
