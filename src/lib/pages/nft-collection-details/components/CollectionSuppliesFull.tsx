91e991789ad72996c8b9fed1c896a3a6b5ce4394
evilpeach <kanisorn.peach@gmail.com>
2025-07-24 12:43:00 +0700

    feat(components): enhance paginator to support total data tracking

--- src/lib/pages/nft-collection-details/components/CollectionSuppliesFull.tsx
+++ src/lib/pages/nft-collection-details/components/CollectionSuppliesFull.tsx
@@ -33,6 +33,7 @@ export const CollectionSuppliesFull = ({
     pagesQuantity,
     setCurrentPage,
     setPageSize,
+    setTotalData,
   } = usePaginator({
     initialState: {
       currentPage: 1,
@@ -45,7 +46,10 @@ export const CollectionSuppliesFull = ({
     collectionAddressHex,
     pageSize,
     offset,
-    debouncedSearch
+    debouncedSearch,
+    {
+      onSuccess: () => setTotalData(totalSupply),
+    }
   );
 
   useEffect(() => setCurrentPage(1), [debouncedSearch, setCurrentPage]);
