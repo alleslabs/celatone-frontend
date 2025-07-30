40bfe87d241e782cae04ca1dd548ca99146db93e
Poafs1 <prach.yot@gmail.com>
2025-07-30 17:04:06 +0700

    feat: rebase

--- src/lib/pages/evm-tx-details/components/EvmTxMsgDetails.tsx
+++ src/lib/pages/evm-tx-details/components/EvmTxMsgDetails.tsx
@@ -1,7 +1,17 @@
 import type { TxData, TxDataJsonRpc } from "lib/services/types";
 import type { Option } from "lib/types";
 
-import { Alert, AlertDescription, Flex, Text } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertDescription,
+  Flex,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { CustomTab } from "lib/components/CustomTab";
 import { DividerWithArrow } from "lib/components/DividerWithArrow";
 import { CustomIcon } from "lib/components/icon";
 import { useEvmVerifyInfos } from "lib/services/verification/evm";
@@ -27,40 +37,63 @@ export const EvmTxMsgDetails = ({
   );
 
   return (
-    <Flex direction="column" flex={1} gap={4} w="full">
-      {cosmosTxData.isTxFailed && (
-        <Alert alignItems="center" mb={2} overflow="unset" variant="error">
-          <Flex align="start" gap={2}>
-            <CustomIcon
-              boxSize={4}
-              color="error.main"
-              name="alert-triangle-solid"
-            />
-            <AlertDescription wordBreak="break-word">
-              {cosmosTxData.rawLog}
-            </AlertDescription>
+    <Tabs isLazy lazyBehavior="keepMounted" w="full">
+      <TabList
+        id="cosmos-evm-txs-tab-list"
+        borderBottomWidth="1px"
+        borderColor="gray.700"
+        overflowX="scroll"
+      >
+        <CustomTab>Overview</CustomTab>
+        <CustomTab>Internal txs</CustomTab>
+        <CustomTab>Balance changes</CustomTab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <Flex direction="column" flex={1} gap={4} w="full">
+            {cosmosTxData.isTxFailed && (
+              <Alert
+                alignItems="center"
+                mb={2}
+                overflow="unset"
+                variant="error"
+              >
+                <Flex align="start" gap={2}>
+                  <CustomIcon
+                    boxSize={4}
+                    color="error.main"
+                    name="alert-triangle-solid"
+                  />
+                  <AlertDescription wordBreak="break-word">
+                    {cosmosTxData.rawLog}
+                  </AlertDescription>
+                </Flex>
+              </Alert>
+            )}
+            <EvmTxMsgDetailsBody evmDenom={evmDenom} evmTxData={evmTxData} />
+            <EvmInputData txInput={evmTxData.tx.input} txTo={evmTxData.tx.to} />
+            {!!evmTxData.txReceipt.logs.length && (
+              <>
+                <DividerWithArrow />
+                <Text color="text.dark" fontWeight={500} variant="body2">
+                  {plur("Event log", evmTxData.txReceipt.logs.length)}
+                </Text>
+                <Flex direction="column" gap={3} w="full">
+                  {evmTxData.txReceipt.logs.map((log) => (
+                    <EvmEventBox
+                      key={log.logIndex.toString()}
+                      evmVerifyInfo={data?.[log.address] ?? null}
+                      log={log}
+                    />
+                  ))}
+                </Flex>
+              </>
+            )}
           </Flex>
-        </Alert>
-      )}
-      <EvmTxMsgDetailsBody evmDenom={evmDenom} evmTxData={evmTxData} />
-      <EvmInputData txInput={evmTxData.tx.input} txTo={evmTxData.tx.to} />
-      {!!evmTxData.txReceipt.logs.length && (
-        <>
-          <DividerWithArrow />
-          <Text color="text.dark" fontWeight={500} variant="body2">
-            {plur("Event log", evmTxData.txReceipt.logs.length)}
-          </Text>
-          <Flex direction="column" gap={3} w="full">
-            {evmTxData.txReceipt.logs.map((log) => (
-              <EvmEventBox
-                key={log.logIndex.toString()}
-                evmVerifyInfo={data?.[log.address] ?? null}
-                log={log}
-              />
-            ))}
-          </Flex>
-        </>
-      )}
-    </Flex>
+        </TabPanel>
+        <TabPanel>Internal txs</TabPanel>
+        <TabPanel>Balance changes</TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 };
--- src/lib/pages/evm-tx-details/components/evm-tx-method/EvmTxDefault.tsx
+++ src/lib/pages/evm-tx-details/components/evm-tx-method/EvmTxDefault.tsx
@@ -1,6 +1,6 @@
 import type { TxDataJsonRpc } from "lib/services/types";
 
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import { ExplorerLink } from "lib/components/ExplorerLink";
 
 import { EvmInfoLabelValue } from "./EvmInfoLabelValue";
@@ -10,41 +10,36 @@ interface EvmTxDefaultProps {
 }
 
 export const EvmTxDefault = ({ evmTxData }: EvmTxDefaultProps) => (
-  <>
-    <Heading as="h6" variant="h6">
-      Sender / Receiver
-    </Heading>
-    <Flex direction="column" gap={4}>
-      <EvmInfoLabelValue
-        label="From"
-        value={
+  <Flex direction="column" gap={4}>
+    <EvmInfoLabelValue
+      label="From"
+      value={
+        <ExplorerLink
+          fixedHeight={false}
+          showCopyOnHover
+          textFormat="normal"
+          type="user_address"
+          value={evmTxData.tx.from}
+        />
+      }
+    />
+    <EvmInfoLabelValue
+      label="To"
+      value={
+        evmTxData.txReceipt.to ? (
           <ExplorerLink
             fixedHeight={false}
             showCopyOnHover
             textFormat="normal"
-            type="user_address"
-            value={evmTxData.tx.from}
+            type="evm_contract_address"
+            value={evmTxData.txReceipt.to}
           />
-        }
-      />
-      <EvmInfoLabelValue
-        label="To"
-        value={
-          evmTxData.txReceipt.to ? (
-            <ExplorerLink
-              fixedHeight={false}
-              showCopyOnHover
-              textFormat="normal"
-              type="evm_contract_address"
-              value={evmTxData.txReceipt.to}
-            />
-          ) : (
-            <Text color="text.disabled" variant="body2">
-              -
-            </Text>
-          )
-        }
-      />
-    </Flex>
-  </>
+        ) : (
+          <Text color="text.disabled" variant="body2">
+            -
+          </Text>
+        )
+      }
+    />
+  </Flex>
 );
