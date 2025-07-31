97aba6298762634ca5bd7a62c3c0001b47621b8e
Poafs1 <prach.yot@gmail.com>
2025-07-31 17:09:43 +0700

    feat: rebase

--- src/lib/components/table/evm-internal-transactions/EvmInternalTransactionsTableRow.tsx
+++ src/lib/components/table/evm-internal-transactions/EvmInternalTransactionsTableRow.tsx
@@ -16,11 +16,9 @@ import { CustomIcon } from "lib/components/icon";
 import {
   coinToTokenWithValue,
   formatInteger,
-  formatPrice,
   formatUTokenWithPrecision,
   getTokenLabel,
 } from "lib/utils";
-import { isUndefined } from "lodash";
 
 import { TableRow } from "../tableComponents";
 
@@ -50,142 +48,118 @@ export const EvmInternalTransactionTableRow = ({
   );
 
   return (
-    <>
-      <AccordionItem>
-        {({ isExpanded }) => (
-          <>
-            <AccordionButton p={0}>
-              <Grid
-                className="copier-wrapper"
-                cursor="pointer"
-                sx={{
-                  "> div": {
-                    paddingLeft: 0,
-                    paddingRight: 0,
-                  },
-                }}
-                templateColumns={templateColumns}
-                w="full"
-              >
-                {showParentHash && (
-                  <TableRow>
-                    {txHash ? (
-                      <ExplorerLink
-                        showCopyOnHover
-                        type="tx_hash"
-                        value={txHash}
-                      />
-                    ) : (
-                      <Flex>
-                        {Array.from({ length: nestedIndex }).map((_, index) => (
-                          <CustomIcon
-                            key={`${nestedIndex}-${index}`}
-                            color="gray.600"
-                            name="arrow-right"
-                          />
-                        ))}
-                      </Flex>
-                    )}
-                  </TableRow>
-                )}
+    <AccordionItem>
+      {({ isExpanded }) => (
+        <>
+          <AccordionButton p={0}>
+            <Grid
+              className="copier-wrapper"
+              cursor="pointer"
+              sx={{
+                "> div": {
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                },
+              }}
+              templateColumns={templateColumns}
+              w="full"
+            >
+              {showParentHash && (
                 <TableRow>
-                  <ExplorerLink
-                    leftIcon={
-                      <CustomIcon
-                        color="primary.main"
-                        name="contract-address"
-                      />
-                    }
-                    showCopyOnHover
-                    type="contract_address"
-                    value={result.from}
-                  />
-                </TableRow>
-                <TableRow>
-                  <CustomIcon color="gray.600" name="arrow-right" />
-                </TableRow>
-                <TableRow>
-                  <ExplorerLink
-                    leftIcon={
-                      <CustomIcon
-                        color="primary.main"
-                        name="contract-address"
-                      />
-                    }
-                    showCopyOnHover
-                    type="contract_address"
-                    value={result.to}
-                  />
-                </TableRow>
-                <TableRow>
-                  <Flex align="center" gap={1}>
-                    <CustomIcon
-                      boxSize={3}
-                      color="success.main"
-                      name="check-circle-solid"
+                  {txHash ? (
+                    <ExplorerLink
+                      showCopyOnHover
+                      type="tx_hash"
+                      value={txHash}
                     />
-                    <Text>{result.type.toLowerCase()}</Text>
-                    <EvmMethodChip
-                      txInput={result.input}
-                      txTo={result.to}
-                      width="auto"
-                    />
-                  </Flex>
-                </TableRow>
-                <TableRow
-                  alignItems="start"
-                  flexDirection="column"
-                  justifyContent="center"
-                >
-                  <Text color="text.dark" variant="body2">
-                    {formatUTokenWithPrecision(
-                      token.amount,
-                      token.precision ?? 0,
-                      true,
-                      token.precision ? 6 : 0
-                    )}{" "}
-                    {getTokenLabel(token.denom, token.symbol)}
-                  </Text>
-                  {!isUndefined(token.value) && (
-                    <Text variant="body3">({formatPrice(token.value)})</Text>
+                  ) : (
+                    <Flex>
+                      {Array.from({ length: nestedIndex }).map((_, index) => (
+                        <CustomIcon
+                          key={`${nestedIndex}-${index}`}
+                          color="gray.600"
+                          name="arrow-right"
+                        />
+                      ))}
+                    </Flex>
                   )}
                 </TableRow>
-                <TableRow>
-                  <Text color="text.dark" variant="body2">
-                    {formatInteger(result.gas.toString())}
-                  </Text>
-                </TableRow>
-                <TableRow justifyContent="flex-end">
+              )}
+              <TableRow>
+                <ExplorerLink
+                  leftIcon={
+                    <CustomIcon color="primary.main" name="contract-address" />
+                  }
+                  showCopyOnHover
+                  type="contract_address"
+                  value={result.from}
+                />
+              </TableRow>
+              <TableRow>
+                <CustomIcon color="gray.600" name="arrow-right" />
+              </TableRow>
+              <TableRow>
+                <ExplorerLink
+                  leftIcon={
+                    <CustomIcon color="primary.main" name="contract-address" />
+                  }
+                  showCopyOnHover
+                  type="contract_address"
+                  value={result.to}
+                />
+              </TableRow>
+              <TableRow>
+                <Flex align="center" gap={1}>
                   <CustomIcon
-                    color="gray.600"
-                    name="chevron-down"
-                    transform={isExpanded ? "rotate(-180deg)" : "rotate(0deg)"}
-                    transition="all 0.25s ease-in-out"
+                    boxSize={3}
+                    color="success.main"
+                    name="check-circle-solid"
                   />
-                </TableRow>
-              </Grid>
-            </AccordionButton>
-            <AccordionPanel>
-              <Text color="text.secondary" variant="body2">
-                Internal call details can go here (if any).
-              </Text>
-            </AccordionPanel>
-          </>
-        )}
-      </AccordionItem>
-      {/* // TODO: Reopen it again when integrate with real data */}
-      {/* {result.calls?.map((call, index) => (
-        <EvmInternalTransactionTableRow
-          key={`${txHash ?? "nested"}-${nestedIndex}-${index}`}
-          assetInfos={assetInfos}
-          evmDenom={evmDenom}
-          nestedIndex={nestedIndex + 1}
-          result={call}
-          showParentHash={showParentHash}
-          templateColumns={templateColumns}
-          txHash={undefined}
-        />
-      ))} */}
-    </>
+                  <Text>{result.type.toLowerCase()}</Text>
+                  <EvmMethodChip
+                    txInput={result.input}
+                    txTo={result.to}
+                    width="auto"
+                  />
+                </Flex>
+              </TableRow>
+              <TableRow
+                alignItems="start"
+                flexDirection="column"
+                justifyContent="center"
+              >
+                <Text color="text.dark" variant="body2">
+                  {formatUTokenWithPrecision(
+                    token.amount,
+                    token.precision ?? 0,
+                    true,
+                    token.precision ? 6 : 0
+                  )}{" "}
+                  {getTokenLabel(token.denom, token.symbol)}
+                </Text>
+              </TableRow>
+              <TableRow>
+                <Text color="text.dark" variant="body2">
+                  {formatInteger(result.gas.toString())}
+                </Text>
+              </TableRow>
+              <TableRow justifyContent="flex-end">
+                <CustomIcon
+                  color="gray.600"
+                  name="chevron-down"
+                  transform={isExpanded ? "rotate(-180deg)" : "rotate(0deg)"}
+                  transition="all 0.25s ease-in-out"
+                />
+              </TableRow>
+            </Grid>
+          </AccordionButton>
+          <AccordionPanel>
+            <Text color="text.secondary" variant="body2">
+              Internal call details can go here (if any).
+            </Text>
+          </AccordionPanel>
+        </>
+      )}
+    </AccordionItem>
   );
 };
