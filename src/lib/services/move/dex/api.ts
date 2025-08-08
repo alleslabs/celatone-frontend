7d4c1e609da369b5265b481c76b94d8350c06576
evilpeach <kanisorn.peach@gmail.com>
2025-08-08 11:19:03 +0700

    fix: incorrect search params

--- src/lib/services/move/dex/api.ts
+++ src/lib/services/move/dex/api.ts
@@ -7,10 +7,8 @@ export const getMoveDexPoolsInfo = () =>
   axios
     .get(`${INITIA_DEX_API}/indexer/dex/v1/pools`, {
       params: {
-        pagination: {
-          count_total: true,
-          limit: 100,
-        },
+        "pagination.count_total": true,
+        "pagination.limit": 100,
       },
     })
     .then(({ data }) => parseWithError(zDexApiPoolsResponse, data));
