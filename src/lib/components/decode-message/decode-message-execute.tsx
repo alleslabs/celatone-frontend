15e0e9dfbf66471fc71cdf7e021505d84d500557
Poafs1 <prach.yot@gmail.com>
2025-08-15 13:59:19 +0700

    feat(utils): add move decode message execute info

--- src/lib/components/decode-message/decode-message-execute.tsx
+++ src/lib/components/decode-message/decode-message-execute.tsx
@@ -1,27 +1,68 @@
 import type { Log } from "@cosmjs/stargate/build/logs";
-import type { MessageResponse, Option } from "lib/types";
+import type { MsgBodyWithoutType } from "lib/utils";
 
-import { Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import { fromBase64 } from "@cosmjs/encoding";
+import { resolveBcsType } from "@initia/utils";
 import { ExplorerLink } from "lib/components/ExplorerLink";
 import JsonReadOnly from "lib/components/json/JsonReadOnly";
+import { useModuleByAddressRest } from "lib/services/move/module";
+import { type MessageResponse, type Option, zAddr } from "lib/types";
 import { extractTxDetails } from "lib/utils";
+import { useMemo, useState } from "react";
 
+import { SelectInput } from "../forms";
 import { DecodeMessageRow } from "./decode-message-row";
 
-interface DecodeMessageExecuteProps {
+const options = [
+  {
+    label: "Decoded",
+    value: "decoded",
+  },
+  {
+    label: "Raw",
+    value: "raw",
+  },
+];
+
+interface DecodeMessageExecuteBodyProps {
+  body: MsgBodyWithoutType;
   log: Option<Log>;
-  msgBody: MessageResponse;
+  type: "/initia.move.v1.MsgExecute";
 }
 
-export const DecodeMessageExecute = ({
+const DecodeMessageExecuteBody = ({
+  body,
   log,
-  msgBody,
-}: DecodeMessageExecuteProps) => {
-  const { "@type": type, ...body } = msgBody;
-
-  if (type !== "/initia.move.v1.MsgExecute") return null;
-
+  type,
+}: DecodeMessageExecuteBodyProps) => {
+  const [value, setValue] = useState<string | undefined>(options[0].value);
   const details = extractTxDetails(type, body, log);
+
+  const { data } = useModuleByAddressRest({
+    address: zAddr.parse(details.module_address),
+    moduleName: details.module_name,
+  });
+
+  const fn = useMemo(
+    () =>
+      [...(data?.executeFunctions ?? []), ...(data?.viewFunctions ?? [])].find(
+        (func) => func.name === details.function_name
+      ),
+    [data, details.function_name]
+  );
+
+  const params =
+    fn?.params[0] === "signer" || fn?.params[0] === "&signer"
+      ? fn.params.slice(1)
+      : fn?.params;
+
+  const text =
+    value === "decoded" && params?.length
+      ? params?.map((param, index) =>
+          resolveBcsType(param).parse(fromBase64(details.args[index]))
+        )
+      : details.args;
 
   return (
     <>
@@ -57,14 +98,49 @@ export const DecodeMessageExecute = ({
           text={JSON.stringify(details.type_args, null, 2)}
         />
       </DecodeMessageRow>
-      <DecodeMessageRow title="Args">
+      <DecodeMessageRow
+        title={
+          <Flex align="center" gap={2}>
+            <Text color="text.dark" variant="body2">
+              Args
+            </Text>
+            <SelectInput
+              classNamePrefix="chakra-react-select"
+              menuPortalTarget={document.body}
+              options={options}
+              placeholder=""
+              size="sm"
+              value={options.find(
+                ({ value: optionValue }) => optionValue === value
+              )}
+              onChange={(newValue) => setValue(newValue?.value)}
+            />
+          </Flex>
+        }
+      >
         <JsonReadOnly
           canCopy
           fullWidth
           isExpandable
-          text={JSON.stringify(details.args, null, 2)}
+          text={JSON.stringify(text, null, 2)}
         />
       </DecodeMessageRow>
     </>
   );
+};
+
+interface DecodeMessageExecuteProps {
+  log: Option<Log>;
+  msgBody: MessageResponse;
+}
+
+export const DecodeMessageExecute = ({
+  log,
+  msgBody,
+}: DecodeMessageExecuteProps) => {
+  const { "@type": type, ...body } = msgBody;
+
+  if (type !== "/initia.move.v1.MsgExecute") return null;
+
+  return <DecodeMessageExecuteBody body={body} log={log} type={type} />;
 };
