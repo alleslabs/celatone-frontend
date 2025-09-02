d22e6a294e48188dba9bf57481fe1916d71e30c4
Poafs1 <prach.yot@gmail.com>
2025-09-02 11:24:31 +0700

    fix(utils): use cosmos evm txs

--- src/lib/components/cosmos-evm-txs/data.ts
+++ src/lib/components/cosmos-evm-txs/data.ts
@@ -6,79 +6,83 @@ import {
   useEvmTxsDataJsonRpc,
   useTxsByAddressPaginationSequencer,
 } from "lib/services/tx";
-import { isUndefined } from "lodash";
 import { useEffect, useState } from "react";
 
 export const useCosmosEvmTxs = (address: BechAddr20) => {
-  const [isInitialLoading, setIsInitialLoading] = useState(true);
-
-  const [evmTxs, setEvmTxs] = useState<TxDataWithTimeStampJsonRpc[]>();
-  const [paginationKey, setPaginationKey] = useState<Nullish<string>>();
+  const [evmTxs, setEvmTxs] = useState<TxDataWithTimeStampJsonRpc[]>([]);
+  const [paginationKey, setPaginationKey] =
+    useState<Nullish<string>>(undefined);
   const [cosmosTxsCount, setCosmosTxsCount] = useState(0);
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
 
   const {
     data: cosmosTxs,
     isError: isCosmosTxsError,
     isFetching: isCosmosTxsFetching,
-    refetch,
   } = useTxsByAddressPaginationSequencer(
     address,
     paginationKey ?? undefined,
     10,
-    evmTxs === undefined
+    isInitialLoading
   );
+
   const {
     data: newEvmTxHashes,
     isError: isNewEvmHashesError,
     isFetching: isNewEvmHashesFetching,
   } = useEvmTxHashesByCosmosTxHashes(
-    cosmosTxs?.items.map((tx) => tx.hash),
-    !isCosmosTxsFetching
+    cosmosTxs?.items.map((tx) => tx.hash) ?? [],
+    !!cosmosTxs && !isCosmosTxsFetching
   );
+
   const {
     data: newEvmTxsData,
     isError: isNewEvmTxsDataError,
     isFetching: isNewEvmTxsDataFetching,
   } = useEvmTxsDataJsonRpc(
-    newEvmTxHashes?.filter((tx) => tx !== null) as string[],
-    !isCosmosTxsFetching && !isNewEvmHashesFetching
+    (newEvmTxHashes ?? []).filter((tx) => tx !== null) as string[],
+    !!newEvmTxHashes && !isNewEvmHashesFetching
   );
 
   useEffect(() => {
-    if (
-      isUndefined(cosmosTxs) ||
-      isUndefined(newEvmTxHashes) ||
-      isUndefined(newEvmTxsData) ||
-      paginationKey === null
-    )
-      return;
+    if (!cosmosTxs || !newEvmTxHashes || !newEvmTxsData) return;
 
-    const newEvmTxs: TxDataWithTimeStampJsonRpc[] = [];
+    const pageEvmTxs: TxDataWithTimeStampJsonRpc[] = [];
     newEvmTxHashes.forEach((evmTxHash, index) => {
-      if (evmTxHash !== null)
-        newEvmTxs.push({
-          ...newEvmTxsData[newEvmTxs.length],
+      if (evmTxHash !== null && newEvmTxsData[index]) {
+        pageEvmTxs.push({
+          ...newEvmTxsData[index],
           timestamp: cosmosTxs.items[index].created,
         });
+      }
     });
 
-    setEvmTxs((evmTxs ?? []).concat(newEvmTxs));
-    setCosmosTxsCount(cosmosTxsCount + cosmosTxs.items.length);
-    setPaginationKey(cosmosTxs.pagination.nextKey);
+    setEvmTxs((prev) =>
+      paginationKey ? [...prev, ...pageEvmTxs] : pageEvmTxs
+    );
+    setCosmosTxsCount((prev) =>
+      paginationKey ? prev + cosmosTxs.items.length : cosmosTxs.items.length
+    );
     setIsInitialLoading(false);
+  }, [cosmosTxs, newEvmTxHashes, newEvmTxsData, paginationKey]);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [newEvmTxsData]);
+  const fetchNextPage = () => {
+    if (cosmosTxs?.pagination?.nextKey) {
+      setPaginationKey(cosmosTxs.pagination.nextKey);
+      setIsInitialLoading(true);
+    }
+  };
 
   const isFetching =
     isCosmosTxsFetching || isNewEvmHashesFetching || isNewEvmTxsDataFetching;
   const isError =
     isCosmosTxsError || isNewEvmHashesError || isNewEvmTxsDataError;
+
   return {
     cosmosTxsCount,
     data: evmTxs,
-    fetchNextPage: refetch,
-    hasNextPage: paginationKey !== null,
+    fetchNextPage,
+    hasNextPage: !!cosmosTxs?.pagination?.nextKey,
     isError,
     isFetchingNextPage: isFetching,
     isLoading: isInitialLoading && !isError,
